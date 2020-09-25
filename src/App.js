150154292ffb694ac1d8d01f5e2ec118aa1db3eb
Yaxue Guo <yaxue.guo23@gmail.com>
2020-09-25 11:09:28 -0400

    #1: added local state to headerNav class and included all main navigation items

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import User from "./pages/User";
+import Resources from "./pages/Resources";
 import Projects from "./pages/Projects";
+import Experiments from "./pages/Experiments";
+import Guide from "./pages/Guide";
+import Links from "./pages/Links";
+import User from "./pages/User";
+import NotFound from "./pages/NotFound";
 import HeaderNav from "./components/headerNav";
 import Footer from "./components/footer";
+
 import "./App.css";
 
 function App() {
@@ -14,15 +20,14 @@ function App() {
       <Router>
         <HeaderNav />
         <Switch>
-          <Route path="/projects">
-            <Projects />
-          </Route>
-          <Route path="/user">
-            <User />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
+          <Route path="/" component={Home} exact />
+          <Route path="/resources" component={Resources} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/experiments" component={Experiments} exact />
+          <Route path="/guide" component={Guide} exact />
+          <Route path="/links" component={Links} exact />
+          <Route path="/user" component={User} />
+          <Route component={NotFound} />
         </Switch>
       </Router>
       <Footer />
--- src/components/headerNav.jsx
+++ src/components/headerNav.jsx
@@ -3,38 +3,49 @@ import { NavLink } from "react-router-dom";
 
 import logo from "../imgs/fabric-brand.png";
 
-const HeaderNav = () => {
-  return (
-    <nav className="navbar navbar-light bg-light">
-      <ul className="nav">
-        <NavLink className="navbar-brand" to="/">
-          <img
-            src={logo}
-            width="70"
-            height="30"
-            className="d-inline-block align-top"
-            alt=""
-          />
-          FABRIC Portal
-        </NavLink>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/">
-            Home
+class HeaderNav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: "Home", path: "/" },
+        { name: "Resources", path: "/resources" },
+        { name: "Projects", path: "/projects" },
+        { name: "Experiments", path: "/experiments" },
+        { name: "Guide", path: "/guide" },
+        { name: "Links", path: "/links" },
+        { name: "User Profile", path: "/user" },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-light">
+        <ul className="nav">
+          <NavLink className="navbar-brand" to="/">
+            <img
+              src={logo}
+              width="70"
+              height="30"
+              className="d-inline-block align-top"
+              alt=""
+            />
+            FABRIC Portal
           </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/projects">
-            Projects
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/user">
-            User Profile
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  );
-};
+          {this.state.data.map((value, index) => {
+            return (
+              <li className="nav-item" key={index}>
+                <NavLink className="nav-link" to={value.path}>
+                  {value.name}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    );
+  }
+}
 
 export default HeaderNav;
