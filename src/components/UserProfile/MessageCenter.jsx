adac830b1bfc5ae9da2d8520ceb194f8e4cc5c18
Yaxue Guo <37635744+yaxue1123@users.noreply.github.com>
2020-10-09 03:05:16 -0400

    Feature/4 user profile side nav (#5)
    * #4: added static sidenav and modifies initial style
    
    * #4: implemented dynamic sidenav and handled click events to change main content component
    
    * #4: modified sidenav color to follow the prototype design

---
+++ src/components/UserProfile/MessageCenter.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MessageCenter = () => {
+  return (
+    <div className="col-9">
+      <h1>MessageCenter</h1>
+    </div>
+  );
+};
+
+export default MessageCenter;
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import SideNav from "../components/SideNav";
+import AccountInfo from "../components/UserProfile/AccountInfo";
+import MyRoles from "../components/UserProfile/MyRoles";
+import MessageCenter from "../components/UserProfile/MessageCenter";
 
-const User = () => {
-  return <div>User Profile</div>;
-};
+class User extends React.Component {
+  state = {
+    SideNavItems: [
+      { name: "ACCOUNT INFORMATION", active: true },
+      { name: "MY ROLES", active: false },
+      { name: "MESSAGE CENTER", active: false },
+    ],
+    activeIndex: 0,
+    componentNames: [AccountInfo, MyRoles, MessageCenter],
+  };
+
+  handleChange = (newIndex) => {
+    this.setState({ activeIndex: newIndex });
+  };
+
+  render() {
+    const TagName = this.state.componentNames[this.state.activeIndex];
+
+    return (
+      <div className="container">
+        <div className="row">
+          <SideNav
+            items={this.state.SideNavItems}
+            handleChange={this.handleChange}
+          />
+          <TagName />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default User;
