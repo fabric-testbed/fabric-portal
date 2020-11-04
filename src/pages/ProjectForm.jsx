cc6fdc62cc0165056f1328d7b843a1a22848d80f
Yaxue Guo <yaxue.guo23@gmail.com>
2020-11-03 22:58:02 -0500

    #15: added long project details to single project page

--- src/pages/ProjectForm.jsx
+++ src/pages/ProjectForm.jsx
@@ -7,8 +7,13 @@ class projectForm extends Form {
   state = {
     data: {
       name: "",
+      _id: "",
       description: "",
       facility: "",
+      created_by: {},
+      created_time: "",
+      project_members: [],
+      project_owners: [],
     },
     errors: {},
   };
@@ -45,19 +50,90 @@ class projectForm extends Form {
       name: project.name,
       description: project.description,
       facility: project.facility,
+      created_by: project.created_by,
+      created_time: project.created_time,
+      project_owners: project.project_owners,
+      project_members: project.project_members,
     };
   }
 
   render() {
     return (
-      <div>
-        <h1>Project Form</h1>
+      <div className="container">
+        <h1>Project - {this.state.data.name}</h1>
         <form onSubmit={this.handleSubmit}>
           {this.renderInput("name", "Name")}
           {this.renderInput("description", "Description")}
           {this.renderInput("facility", "Facility")}
           {this.renderButton("Save")}
         </form>
+        <table className="table table-striped table-bordered w-50 my-4">
+          <tbody>
+            <tr>
+              <td>Project ID</td>
+              <td>{this.state.data._id}</td>
+            </tr>
+          </tbody>
+        </table>
+        <h2 className="my-4">Creator Information</h2>
+        <table className="table table-striped table-bordered w-50">
+          <tbody>
+            <tr>
+              <td>Name</td>
+              <td>{this.state.data.created_by.name}</td>
+            </tr>
+            <tr>
+              <td>Email</td>
+              <td>{this.state.data.created_by.email}</td>
+            </tr>
+            <tr>
+              <td>Creator ID</td>
+              <td>{this.state.data.created_by.uuid}</td>
+            </tr>
+            <tr>
+              <td>Created Time</td>
+              <td>{this.state.data.created_time}</td>
+            </tr>
+          </tbody>
+        </table>
+        <h2 className="my-4">Project Owners</h2>
+        <table className="table table-striped table-bordered w-75">
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>ID</th>
+            </tr>
+            {this.state.data.project_owners.map((owner, index) => {
+              return (
+                <tr key={index}>
+                  <td>{owner.name}</td>
+                  <td>{owner.email}</td>
+                  <td>{owner.uuid}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <h2 className="my-4">Project Members</h2>
+        <table className="table table-striped table-bordered w-75">
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>ID</th>
+            </tr>
+            {this.state.data.project_members.map((member, index) => {
+              return (
+                <tr key={index}>
+                  <td>{member.name}</td>
+                  <td>{member.email}</td>
+                  <td>{member.uuid}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     );
   }
