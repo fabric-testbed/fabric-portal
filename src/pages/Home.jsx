b73c489fdbebac7f12da4204a861ff9e4484c251
Yaxue Guo <yaxue.guo23@gmail.com>
2020-11-06 11:00:39 -0500

    #16: added static lower part of home page - unlogged in, one column for Facility Update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,41 @@ class Home extends React.Component {
             <button className="btn btn-warning">Learn More</button>
           </div>
         </div>
-        <div className="home-lower"></div>
+        <div className="home-lower">
+          <div class="card">
+            <div class="card-header">
+              <b>Facility Update</b>
+            </div>
+            <div class="card-body">
+              <div className="update-1 pb-4 border-bottom">
+                <h6 class="card-title">2020-11-06</h6>
+                <h5 class="card-title">Title for the update</h5>
+                <p class="card-text">
+                  Pellentesque habitant morbi tristique senectus et netus et
+                  malesuada fames ac turpis egestas. Vestibulum tortor quam,
+                  feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu
+                  libero sit amet quam egestas semper...
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Read More
+                </a>
+              </div>
+              <div className="update-1 pt-4">
+                <h6 class="card-title">2020-11-06</h6>
+                <h5 class="card-title">Title for the update</h5>
+                <p class="card-text">
+                  Pellentesque habitant morbi tristique senectus et netus et
+                  malesuada fames ac turpis egestas. Vestibulum tortor quam,
+                  feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu
+                  libero sit amet quam egestas semper...
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Read More
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
