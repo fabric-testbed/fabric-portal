307d82fc5af995c4e60c018d5f65fc9fa5c56e62
Yaxue Guo <yaxue.guo23@gmail.com>
2020-11-09 12:42:54 -0500

    #16: applied fake facility update data to homepage

--- src/services/fakeFacilityUpdate.js
+++ src/services/fakeFacilityUpdate.js
@@ -1,21 +1,21 @@
 export const updates = [
   {
-    update_id: 1,
-    subject: "Title for Update 1",
+    id: 1,
+    title: "Title for Update 1",
     content:
       "Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth.",
     date: "2020-11-01",
   },
   {
-    update_id: 2,
-    subject: "Title for 2",
+    id: 2,
+    title: "Title for Update 2",
     content:
       "Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth.",
     date: "2020-11-05",
   },
   {
-    update_id: 3,
-    subject: "Title for 3",
+    id: 3,
+    title: "Title for Update 3",
     content:
       "Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth.",
     date: "2020-11-06",
