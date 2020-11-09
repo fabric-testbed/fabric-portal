37bd819808fd987b9f9b6fd451cefd75c46851d7
Yaxue Guo <yaxue.guo23@gmail.com>
2020-11-09 12:24:16 -0500

    #16: separated homepage introduction text data with UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,15 @@
 import React from "react";
+import { homepageIntro } from "../services/portalData.json";
+import { getUpdates } from "../services/fakeFacilityUpdate";
 
 class Home extends React.Component {
   render() {
     return (
       <div className="home-container">
         <div className="home-upper">
-          <div
-            className="home-upper-text"
-            // style={{
-            //   background: `url(${bg})`,
-            // }}
-          >
+          <div className="home-upper-text">
             <h1>FABRIC Portal</h1>
-            <p>
-              Pellentesque habitant morbi tristique senectus et netus et
-              malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat
-              vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
-              amet quam egestas semper. Aenean ultricies mi vitae est. Mauris
-              placerat eleifend leo. fames ac turpis egestas. Vestibulum tortor
-              quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec
-              eu libero sit amet quam egestas semper. Aenean ultricies mi vitae
-              est. Mauris placerat eleifend leo.
-            </p>
+            <p>{homepageIntro}</p>
             <button className="btn btn-warning">Learn More</button>
           </div>
         </div>
