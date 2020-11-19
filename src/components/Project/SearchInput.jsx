b61c2ee7e3ecbb6c74e8403f61295d3cd8c2ce33
Yaxue Guo <yaxue.guo23@gmail.com>
2020-11-19 14:50:30 -0500

    #20: added project owner table in new project page

--- src/components/Project/SearchInput.jsx
+++ src/components/Project/SearchInput.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+
+import ProjectUserTable from "./ProjectUserTable";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { getPeopleByName } from "../../services/userInformationService";
@@ -6,6 +9,14 @@ import { getPeopleByName } from "../../services/userInformationService";
 class SearchInput extends React.Component {
   state = {
     users: [],
+    addedUsers: [],
+    activeTableIndex: 0,
+    ownerSetting: {
+      pageSize: 5,
+      currentPage: 1,
+      searchQuery: "",
+      sortColumn: { path: "name", order: "asc" },
+    },
   };
 
   handleSearch = async (value) => {
@@ -17,7 +28,42 @@ class SearchInput extends React.Component {
     }
   };
 
+  handleAddUser = (user) => {
+    const added = this.state.addedUsers;
+    let found = false;
+    for (let i = 0; i < added.length; i++) {
+      if (added[i].eppn === user.eppn) {
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      added.push(user);
+      this.setState({ addedUsers: added });
+    }
+  };
+
+  handleSort = (sortColumn) => {
+    if (this.state.activeTableIndex === 0) {
+      this.setState({
+        ownerSetting: { ...this.state.ownerSetting, sortColumn: sortColumn },
+      });
+    } else if (this.state.TableactiveIndex === 1) {
+      this.setState({
+        memberSetting: { ...this.state.memberSetting, sortColumn: sortColumn },
+      });
+    }
+  };
+
+  handleDelete = (user) => {
+    // only delete a added user from state, no interaction with api.
+    let added = this.state.addedUsers;
+    added = added.filter((u) => u.eppn !== user.eppn);
+    this.setState({ addedUsers: added });
+  };
+
   render() {
+    const that = this;
     return (
       <div>
         <input
@@ -33,7 +79,7 @@ class SearchInput extends React.Component {
                   {user.name}
                   <button
                     className="btn btn-danger ml-2"
-                    // onClick={this.handleAddUser(user)}
+                    onClick={() => that.handleAddUser(user)}
                   >
                     <FontAwesomeIcon icon={faPlus} />
                   </button>
@@ -41,6 +87,14 @@ class SearchInput extends React.Component {
               );
             })}
           </ul>
+        </div>
+        <div>
+          <ProjectUserTable
+            users={this.state.addedUsers}
+            sortColumn={this.state.ownerSetting.sortColumn}
+            onSort={this.handleSort}
+            onDelete={this.handleDelete}
+          />
         </div>
       </div>
     );
