e0eb703d3db4ed5985df2ecea1f3be16107ab05f
Yaxue Guo <yaxue.guo23@gmail.com>
2020-12-04 00:59:54 -0500

    #21: added login/ logout buttons to navbar

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -27,6 +27,15 @@ class HeaderNav extends React.Component {
       { name: "Links", path: "/links", child: [], exact: false },
       { name: "User Profile", path: "/user", child: [], exact: false },
     ],
+    token: localStorage.getItem("token"),
+  };
+
+  handleLogout = () => () => {
+    // remove auth token in browser.
+    localStorage.removeItem("token");
+
+    // let nginx process logout.
+    window.location.href = "/logout";
   };
 
   render() {
@@ -53,53 +62,86 @@ class HeaderNav extends React.Component {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            {this.state.navItems.map((item, index) => {
-              return (
-                <li
-                  className={
-                    "nav-item" + (item.child.length > 0 ? " dropdown" : "")
-                  }
-                  key={index}
+        {this.state.token === null && (
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item ml-4">
+                <button
+                  onClick={() => {
+                    window.location.href = "/login";
+                  }}
+                  className="btn btn-md btn-warning text-white"
                 >
-                  <NavLink
+                  Login
+                </button>
+              </li>
+            </ul>
+          </div>
+        )}
+        {this.state.token !== null && (
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              {this.state.navItems.map((item, index) => {
+                return (
+                  <li
                     className={
-                      "nav-link" +
-                      (item.child.length > 0 ? " dropdown-toggle" : "")
+                      "nav-item" + (item.child.length > 0 ? " dropdown" : "")
                     }
-                    to={item.path}
-                    id={`navbarDropdownMenuLink-${index}`}
-                    data-toggle={item.child.length > 0 ? "dropdown" : ""}
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                    exact={item.exact}
+                    key={index}
                   >
-                    {item.name}
-                  </NavLink>
-                  {item.child.length > 0 && (
-                    <div
-                      className="dropdown-menu"
-                      aria-labelledby={`navbarDropdownMenuLink-${index}`}
+                    <NavLink
+                      className={
+                        "nav-link" +
+                        (item.child.length > 0 ? " dropdown-toggle" : "")
+                      }
+                      to={item.path}
+                      id={`navbarDropdownMenuLink-${index}`}
+                      data-toggle={item.child.length > 0 ? "dropdown" : ""}
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                      exact={item.exact}
                     >
-                      {item.child.map((sub_item, sub_index) => {
-                        return (
-                          <NavLink
-                            className="dropdown-item"
-                            to={sub_item.path}
-                            key={sub_index}
-                          >
-                            {sub_item.name}
-                          </NavLink>
-                        );
-                      })}
-                    </div>
-                  )}
+                      {item.name}
+                    </NavLink>
+                    {item.child.length > 0 && (
+                      <div
+                        className="dropdown-menu"
+                        aria-labelledby={`navbarDropdownMenuLink-${index}`}
+                      >
+                        {item.child.map((sub_item, sub_index) => {
+                          return (
+                            <NavLink
+                              className="dropdown-item"
+                              to={sub_item.path}
+                              key={sub_index}
+                            >
+                              {sub_item.name}
+                            </NavLink>
+                          );
+                        })}
+                      </div>
+                    )}
+                  </li>
+                );
+              })}
+              <React.Fragment>
+                <li className="nav-item ml-4">
+                  <button
+                    onClick={this.handleLogout()}
+                    className="btn btn-md btn-warning text-white"
+                  >
+                    Logout
+                  </button>
                 </li>
-              );
-            })}
-          </ul>
-        </div>
+              </React.Fragment>
+            </ul>
+          </div>
+        )}
       </nav>
     );
   }
