e4561c46eac6e625528ae2e73f0af994a2da247d
Yaxue Guo <37635744+yaxue1123@users.noreply.github.com>
2020-12-17 18:09:43 -0500

    Feature/30 enrich form subcomponents (#31)
    * #30: added select subcomponents to form and applied to New Project page
    
    * #30: applied select for facility field on single project page
    
    * #30: added textarea subcomponents for form and applied to the description field
    
    * #30: fixed select render issue for single project

--- src/pages/ProjectForm.jsx
+++ src/pages/ProjectForm.jsx
@@ -9,6 +9,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 import { getPeopleByName } from "../services/userInformationService";
+import { facilityOptions } from "../services/portalData.json";
+
 import {
   getProject,
   saveProject,
@@ -308,8 +310,8 @@ class projectForm extends Form {
             >
               <form onSubmit={this.handleSubmit}>
                 {this.renderInput("name", "Name")}
-                {this.renderInput("description", "Description")}
-                {this.renderInput("facility", "Facility")}
+                {this.renderTextarea("description", "Description")}
+                {this.renderSelect("facility", "Facility", this.state.data.facility, facilityOptions)}
                 {this.renderInputTag("tags", "Tags")}
                 {this.renderButton("Save")}
               </form>
--- src/services/fakeFacilityService.js
+++
@@ -1,5 +0,0 @@
-export const facilities = [{ _id: 1, name: "FABRIC" }];
-
-export function getFacilities() {
-  return facilities;
-}
