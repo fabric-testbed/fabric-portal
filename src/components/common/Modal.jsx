65eebb3cfc6b4d722fb3aad99e2af28d078ce593
Yaxue Guo <yaxue.guo23@gmail.com>
2021-02-16 15:22:56 -0500

    #45: added link to request button

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -1,55 +1,65 @@
 import React from "react";
 
-const Modal = ({ id, title, link, content, error, ...rest }) => {
-  return (
-    <div>
-      <div
-        className="modal fade"
-        id={id}
-        tabIndex="-1"
-        role="dialog"
-        aria-labelledby={`${id}-title`}
-        aria-hidden="true"
-      >
-        <div className="modal-dialog modal-dialog-centered" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id={`${id}-title`}>
-                {title}
-              </h5>
-              <button
-                type="button"
-                className="close"
-                data-dismiss="modal"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div className="modal-body">
-              {content}
-            </div>
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-dismiss="modal"
-              >
-                Cancel
-              </button>
-              <button
-                type="button"
-                className="btn btn-primary"
-              >
-                Next
-              </button>
+class Modal extends React.Component {
+
+  render() {
+    const { id, title, link, content } = this.props;
+    return (
+      <div>
+        <button
+          type="button"
+          className="btn btn-primary"
+          data-toggle="modal"
+          data-target={`#${id}`}
+        >
+          Request to be Project Lead
+        </button>
+        <div
+          className="modal fade"
+          id={id}
+          tabindex="-1"
+          role="dialog"
+          aria-labelledby={`${id}-title`}
+          aria-hidden="true"
+        >
+          <div className="modal-dialog modal-dialog-centered" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id={`${id}-title`}>
+                  {title}
+                </h5>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                {content}
+              </div>
+              <div className="modal-footer">
+                <a href={link}>
+                  <button type="button" className="btn btn-primary">
+                    Next
+                  </button>
+                </a>
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-dismiss="modal"
+                >
+                  Cancel
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      {error && <div className="alert alert-danger">{error}</div>}
-    </div>
-  );
-};
+    )
+  }
+}
 
 export default Modal;
