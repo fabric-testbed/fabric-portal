543a12903c616ba800690592370a4c008f3ae390
Yaxue Guo <yaxue.guo23@gmail.com>
2021-02-23 16:50:52 -0500

    #48: applied loading spinner to new project form

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import LoadSpinner from "../components/common/LoadSpinner";
 
 const Resources = () => {
   return (
   <div className="container">
     Resources
-    <LoadSpinner text={"Creating Project..."} showSpinner={false} />
   </div>
   );
 };
