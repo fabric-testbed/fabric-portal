445fcd528c8a478ca1ac0a1b1e66f1638d966151
Yaxue Guo <yaxue.guo23@gmail.com>
2021-03-03 15:54:33 -0500

    #37: test x-error of /whoami - 2

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -4,20 +4,19 @@ import { toast } from "react-toastify";
 axios.defaults.withCredentials = true;
 
 axios.interceptors.response.use(null, (error) => {
-  console.log("------");
-  console.log("error" + error);
-  console.log("error.response" + error.response);
-  console.log("error.response.status" + error.response.status);
-  console.log("error.response.headers" + error.response.headers);
-  console.log("error.response.headers['Content-Type']" + error.response.headers['Content-Type']);
-  console.log("error.response.headers['X-Error']" + error.response.headers['X-Error']);
-  console.log("------");
+  // console.log("error" + error);
+  // console.log("error.response" + error.response);
+  // console.log("error.response.status" + error.response.status);
+  // console.log("error.response.headers" + error.response.headers);
+  // console.log("error.response.headers['Content-Type']" + error.response.headers['Content-Type']);
+  // console.log("error.response.headers['X-Error']" + error.response.headers['X-Error']);
+
+  toast.error("test error");
+  toast.error(error.response.headers['X-Error']);
 
   if (error.response && error.response.status === 401) {
     // user is not active and need signup
     // do not toast error message.
-    toast.error("test error");
-    toast.error(error.response.headers['X-Error']);
     return Promise.reject(error); 
   }
 
