6ab9854ace5b5c0c4c822d7027b81683bf0082d5
Yaxue Guo <yaxue.guo23@gmail.com>
2021-03-04 15:42:48 -0500

    #37: test axios interceptor response

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -3,40 +3,50 @@ import { toast } from "react-toastify";
 
 axios.defaults.withCredentials = true;
 
-axios.interceptors.response.use(null, (error) => {
-  console.log("error" + error);
-  // console.log("error.response" + error.response);
-  // console.log("error.response.status" + error.response.status);
-  // console.log("error.response.headers" + error.response.headers);
-  // console.log("error.response.headers['Content-Type']" + error.response.headers['Content-Type']);
-  // console.log("error.response.headers['X-Error']" + error.response.headers['X-Error']);
+axios.interceptors.response.use(
+  (response) => {
+    // Any status code that lies within the range of 2xx cause this function.
+    console.log("res" + response);
+    console.log("res.headers" + response.headers);
+    console.log("response.status" + response.status);
+    console.log("res.headers" + response.headers['content-type']);
+    return response;
+  }, 
+  (error) => {
+    // Any status code that falls outside the range of 2xx cause this function.
+    console.log("error" + error);
+    console.log("error.status" + error.status);
+    console.log("error.headers" + error.headers);
+    console.log("error.headers['Content-Type']" + error.headers['Content-Type']);
+    console.log("error.headers['X-Error']" + error.headers['X-Error']);
 
-  toast.error("test error");
-  toast.error(error.response.headers['X-Error']);
+    toast.error("test error");
+    toast.error(error.headers['X-Error']);
 
-  if (error.response && error.response.status === 401) {
-    // user is not active and need signup
-    // do not toast error message.
+    if (error.response && error.response.status === 401) {
+      // user is not active and need signup
+      // do not toast error message.
+      return Promise.reject(error); 
+    }
+
+    const clientError =
+      error.response &&
+      error.response.status >= 400 &&
+      error.response.status < 500;
+    
+    const serverError = error.response && error.response.status >= 500 ;
+
+    if (clientError) {
+      toast.error("A client-side error occurred.");
+    } else if (serverError){
+      toast.error("A server-side error occurred.");
+    } else {
+      toast.error("An unexpected error occurred.");
+    }
+
     return Promise.reject(error); 
   }
-
-  const clientError =
-    error.response &&
-    error.response.status >= 400 &&
-    error.response.status < 500;
-  
-  const serverError = error.response && error.response.status >= 500 ;
-
-  if (clientError) {
-    toast.error("A client-side error occurred.");
-  } else if (serverError){
-    toast.error("A server-side error occurred.");
-  } else {
-    toast.error("An unexpected error occurred.");
-  }
-
-  return Promise.reject(error); 
-});
+);
 
 export default {
   get: axios.get,
