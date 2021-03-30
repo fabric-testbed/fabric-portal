0c51d98ffb8903069d7215f6818092812456a887
Yaxue Guo <yaxue.guo23@gmail.com>
2021-03-30 12:40:48 -0400

    #52: added timeout to axios

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -2,15 +2,14 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 axios.defaults.withCredentials = true;
-// axios.defaults.timeout = 20000;
+axios.defaults.timeout = 3000;
 
 axios.interceptors.response.use(null, (error) => {
-    // Any status code that falls outside the range of 2xx cause this function.
-    console.log("error.response.data: " + error.response.data);
-    console.log("error.response.status" + error.response.status);
-    console.log("error.response.headers keys: " + Object.keys(error.response.headers));
-    console.log("error.response.headers['content-type']" + error.response.headers['content-type']);
-    console.log("error.response.headers['x-error']" + error.response.headers['x-error']);
+
+    if(error.code === 'ECONNABORTED') {
+      toast.error("Request timeout. Please try again.");
+      return Promise.reject(error); 
+    }
 
     if (error.response && error.response.status === 401) {
       // user is not active and need signup
