3462787ee25604547df87948f3bea94b3cbb8dba
Yaxue Guo <yaxue.guo23@gmail.com>
2021-03-30 13:06:05 -0400

    #52: completed timeout handling in axios

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -2,21 +2,21 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 axios.defaults.withCredentials = true;
-axios.defaults.timeout = 3000;
+// set timeout to be 20 seconds
+axios.defaults.timeout = 20000;
 
 axios.interceptors.response.use(null, (error) => {
-
-    if(error.code === 'ECONNABORTED') {
-      toast.error("Request timeout. Please try again.");
-      return Promise.reject(error); 
-    }
-
     if (error.response && error.response.status === 401) {
       // user is not active and need signup
       // do not toast error message.
       return Promise.reject(error); 
     }
 
+    // Timeout error.
+    if(error.code === 'ECONNABORTED') {
+      toast.error("Request timeout. Please try again.");
+    }
+  
     if (error.response && error.response.data) {
       // toast the error message of x-error.
       toast.error(error.response.data);
