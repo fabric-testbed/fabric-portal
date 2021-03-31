f7cf03d2b336cbe17fc700013be30f8fd203cf0c
Yaxue Guo <37635744+yaxue1123@users.noreply.github.com>
2021-03-31 09:15:29 -0400

    Feature/Add Refresh Role Button (#55)
    * #52: added the fab-phase map to resource page
    
    * #52: used same styled links and nodes for international ones
    
    * #52: added zoomable group tp the map & control buttons
    
    * #52: adjusted U.S. nodes position and coordinates to reduce overlap
    
    * #52: completed the basic resource page layout of map + side table + summary table
    
    * #52: added search/ sort/ pagination to summary table
    
    * #52: updated map style && added progress bar to detail table
    
    * #52: implemented dynamic detail table - change site name when hover
    
    * #52: added jupyterhub link to navbar - links
    
    * #52: hide User/Message Center for demo purposes
    
    * #52: replaced mock data/ modified summary table cols/ pass props to detail table
    
    * #52: added testbed sum table on the top
    
    * #52: added sum function to sum up all sites' capacity
    
    * #52: modified table styles
    
    * #52: added hover effect to map nodes
    
    * #52: added homepage intro text
    
    * #52: updated read more link"
    
    * #52: uncommented async calls for homepage
    
    * #52: added real data for facility updates"
    
    * #52: updated app footer to reflect current year
    
    * #52: updated jupyterhub link
    
    * #52: added 3s test timeout to create project
    
    * #52: updated timeout function
    
    * #52: updated timeout function
    
    * #52: set timeout to 20s
    
    * #52: modified axios timeout param
    
    * #52: set axios timeout to 20s
    
    * #52: restore changes
    
    * #52: added timeout to axios
    
    * #52: completed timeout handling in axios
    
    * #52: added refresh roles button on user profile page
    
    * #52: added refresh role calls to PR

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,8 +5,10 @@ import MyRoles from "../components/UserProfile/MyRoles";
 import MessageCenter from "../components/UserProfile/MessageCenter";
 import { toast } from "react-toastify";
 
+import sleep from "../utils/sleep";
+
 import { getWhoAmI } from "../services/userInformationService.js";
-import { getCurrentUser } from "../services/prPeopleService.js";
+import { getCurrentUser, refreshRoles } from "../services/prPeopleService.js";
 
 class User extends React.Component {
   state = {
@@ -36,6 +38,21 @@ class User extends React.Component {
     this.setState({ activeIndex: newIndex });
   };
 
+  handleRoleRefresh = async () => {
+    try {
+      // Refresh role first, then get updated people info.
+      // two async/ await calls in series.
+      await refreshRoles();
+      // wait 1 second for PR updates.
+      await sleep(1000);
+      const { data: people } = await getCurrentUser();
+      this.setState({ people });
+    } catch (ex) {
+      toast.error("Failed to refresh roles. Please try again.");
+      console.log("Failed to refresh roles " + ex.response.data);
+    }
+  }
+
   render() {
     const TagName = this.state.componentNames[this.state.activeIndex];
 
@@ -46,7 +63,11 @@ class User extends React.Component {
             items={this.state.SideNavItems}
             handleChange={this.handleChange}
           />
-          <TagName user={this.state.user} people={this.state.people}/>
+          <TagName
+            user={this.state.user}
+            people={this.state.people}
+            onRoleRefresh={this.handleRoleRefresh}
+          />
         </div>
       </div>
     );
--- src/services/prPeopleService.js
+++ src/services/prPeopleService.js
@@ -8,3 +8,7 @@ export function getCurrentUser() {
   const uuid = localStorage.getItem("userID");
   return axios.get(apiEndpoint + "/" + uuid, config);
 }
+
+export function refreshRoles() {
+  return axios.get(apiEndpoint + "/role_attribute_sync", config);
+}
