48b56743c813810fb0bb91126a2b0a2eea323cad
Yaxue Guo <37635744+yaxue1123@users.noreply.github.com>
2021-07-30 15:28:03 -0400

    Enhancement/57 enrich footer content (#79)
    * #57: added three cols for footer
    
    * #57: added links for social icons
    
    * #57: added internal route for help links
    
    * #57: added responsive breakpoints to footer
    
    * #57: fixed footer and header style
    
    * #57: modified homepage responsive design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,66 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Footer = () => {
-  return <div className="app-footer bg-light">© FABRIC {new Date().getFullYear()}</div>;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+
+class Footer extends React.Component {
+  render() {
+    return (<div className="app-footer bg-light">
+      <div className="row py-4">
+        <div className="col-sm-12 col-md-4">
+          <ul>
+            <li><b>Useful Links</b></li>
+            <li>
+              <a href="https://fabric-testbed.net/" target="_blank" rel="noopener noreferrer">FABRIC Website</a>
+            </li>
+            <li>
+              <Link to="/aup">Acceptable Use Policy</Link>
+            </li>
+            <li><Link to="/cookie-policy">Cookie Policy</Link></li>
+            <li><Link to="/privacy-policy">Privacy Policy</Link></li>
+          </ul>
+        </div>
+        <div className="col-sm-12 col-md-4">
+          <ul>
+            <li><b>Help &amp; Support</b></li>
+            <li><a href="https://learn.fabric-testbed.net/" target="_blank" rel="noopener noreferrer">Knowledge Base</a></li>
+            <li><a href="https://learn.fabric-testbed.net/faq/" target="_blank" rel="noopener noreferrer">FAQ</a></li>
+            <li><a href="https://learn.fabric-testbed.net/submit-a-ticket/" target="_blank" rel="noopener noreferrer">Contact Us</a></li>
+          </ul>
+        </div>
+        <div className="col-sm-12 col-md-4">
+          <ul>
+            <li><b>Social</b></li>
+            <li><div className="d-flex flex-row justify-content-between w-50 footer-social-icons">
+              <a href="https://twitter.com/FABRICtestbed" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faTwitter} />
+              </a>
+              <a href="https://github.com/fabric-testbed" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+              <a href="http://bit.ly/FABRICYouTube" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faYoutube} />
+              </a>
+              <a href="mailto:info@fabric-testbed.net">
+                <FontAwesomeIcon icon={faEnvelope} />
+              </a>
+            </div></li>
+          </ul>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <ul>
+            <li><u><b>© FABRIC {new Date().getFullYear()}</b></u></li>
+          </ul>
+        </div>
+      </div>
+    </div>)
+  }
 };
 
 export default Footer;
