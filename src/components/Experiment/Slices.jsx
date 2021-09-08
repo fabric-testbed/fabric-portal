c5ce071e403a84fec8d614faca59e16017a7326b
Yaxue Guo <yaxue.guo23@gmail.com>
2021-09-08 16:18:13 -0400

    #89: updated Experiments page to three sections

---
+++ src/components/Experiment/Slices.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Pagination from "../common/Pagination";
+import SearchBox from "../common/SearchBox";
+import SlicesTable from "../Slice/SlicesTable";
+
+import { getSlices } from "../../services/fakeSlices.js";
+
+import paginate from "../../utils/paginate";
+import _ from "lodash";
+
+class Slices extends React.Component {
+  state = {
+    slices: getSlices(),
+    allSlices: getSlices(),
+    pageSize: 5,
+    currentPage: 1,
+    searchQuery: "",
+    sortColumn: { path: "name", order: "asc" },
+  };
+
+  async componentDidMount() {
+
+  }
+
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
+
+  handleSearch = (query) => {
+    this.setState({ searchQuery: query, currentPage: 1 });
+  };
+
+  handleSort = (sortColumn) => {
+    this.setState({ sortColumn });
+  };
+
+  getPageData = () => {
+    const {
+      pageSize,
+      currentPage,
+      sortColumn,
+      searchQuery,
+      slices: allSlices,
+    } = this.state;
+
+    // filter -> sort -> paginate
+    let filtered = allSlices;
+    if (searchQuery) {
+      filtered = allSlices.filter((s) =>
+        s.slice_name.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+    }
+
+    const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+
+    const slices = paginate(sorted, currentPage, pageSize);
+
+    return { totalCount: filtered.length, data: slices };
+  };
+
+  render() {
+    const { pageSize, currentPage, sortColumn, searchQuery } = this.state;
+    const { totalCount, data } = this.getPageData();
+
+    return (
+      <div className="container">
+        <h1>Slices</h1>
+        <div className="toolbar">
+          <SearchBox
+            value={searchQuery}
+            placeholder={"Search slices..."}
+            onChange={this.handleSearch}
+            className="my-0"
+          />
+          <Link to="/slices/new" className="btn btn-primary">
+            Create Slice
+          </Link>
+        </div>
+        <div className="my-2">
+          Showing {totalCount} slices.
+        </div>
+        <SlicesTable
+          slices={data}
+          sortColumn={sortColumn}
+          onSort={this.handleSort}
+        />
+        <Pagination
+          itemsCount={totalCount}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={this.handlePageChange}
+        />
+      </div>
+    );
+  }
+}
+
+export default Slices;
---
+++ src/components/Slice/SlicesTable.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Table from "../common/Table";
+
+class SlicesTable extends Component {
+  columns = [
+      {
+        path: "slice_name",
+        label: "Slice Name",
+        content: (slice) => (
+          <Link to={`/slices/${slice.slice_id}`}>{slice.slice_name}</Link>
+        ),
+      },
+      { path: "slice_state", label: "Slice State" },
+      { path: "lease_end", label: "Lease End" },
+    ];
+
+  render() {
+    const { slices, onSort, sortColumn } = this.props;
+    console.log(slices)
+    return (
+      <Table
+        columns={this.columns}
+        data={slices}
+        sortColumn={sortColumn}
+        onSort={onSort}
+      />
+    );
+  }
+}
+
+export default SlicesTable;
