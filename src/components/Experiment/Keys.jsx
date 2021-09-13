d63ab51dc06fb6a0f39d2f148758699cbd482d5e
Yaxue Guo <yaxue.guo23@gmail.com>
2021-09-13 12:01:44 -0400

    #89 & #82: added generate/ upload ssh key UI components

--- src/components/Experiment/Keys.jsx
+++ src/components/Experiment/Keys.jsx
@@ -1,8 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Pagination from "../common/Pagination";
 import SearchBox from "../common/SearchBox";
 import KeysTable from "./KeysTable";
+import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
+import FormControl from "react-bootstrap/FormControl";
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
 
 import { getKeys } from "../../services/fakeSSHKeys.js";
 
@@ -73,9 +77,6 @@ class Keys extends React.Component {
             onChange={this.handleSearch}
             className="my-0"
           />
-          <Link to="/keys/new" className="btn btn-primary">
-            Generate Key
-          </Link>
         </div>
         <div className="my-2">
           Showing {totalCount} keys.
@@ -91,6 +92,42 @@ class Keys extends React.Component {
           currentPage={currentPage}
           onPageChange={this.handlePageChange}
         />
+        <h3 className="my-4">Generate SSH Key Pair</h3>
+        <InputGroup className="mb-3">
+          <FormControl
+            placeholder="Input the Key Pair Name..."
+            aria-label="SSH Key Name"
+          />
+          <Button className="ml-4" variant="success">
+            Generate Key Pair
+          </Button>
+        </InputGroup>
+        <h3 className="my-4">Upload Public Key</h3>
+        <Card>
+          <Card.Header className="d-flex flex-row bg-light">
+            Please paste the public key here:
+          </Card.Header>
+          <Card.Body>
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+              <Form.Control
+                ref={(textarea) => this.textArea = textarea}
+                as="textarea"
+                id="createTokenTextArea"
+                defaultValue={this.state.createToken}
+                rows={6}
+              />
+            </Form.Group>
+          </Card.Body>
+        </Card>
+        <InputGroup className="my-3">
+          <FormControl
+            placeholder="Input the Public Key Name..."
+            aria-label="SSH Key Name"
+          />
+          <Button className="ml-4" variant="success">
+          Upload Public Key
+          </Button>
+        </InputGroup>
       </div>
     );
   }
--- src/components/Experiment/KeysTable.jsx
+++ src/components/Experiment/KeysTable.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import Table from "../common/Table";
 
 class KeysTable extends Component {
