e94700ec49e26e3314188007607265a50f18bb5f
Yaxue Guo <yaxue.guo23@gmail.com>
2021-10-06 12:04:43 -0400

    #78: solved exceptional status for OVS has SmartNIC

--- src/services/parser/sliceParser.js
+++ src/services/parser/sliceParser.js
@@ -66,6 +66,14 @@ export default function parseSlice(abqm) {
   const findParentNode = (id) => {
     let parentId = -1;
     for (let i = 0; i < links.length; i++) {
+      // EXCEPTION status: OVS has SmartNIC (usually SmartNIC has OVS)
+      if (links[i].label === "has" && objNodes[links[i].target].Type === "SmartNIC" && 
+      objNodes[links[i].source].Type === "OVS" && links[i].source === id) {
+        parentId = objNodes[links[i].target].id;
+        break;
+      }
+
+      // Normal status:
       if (links[i].label === "has" && links[i].target === id) {
         parentId = links[i].source;
         break;
@@ -106,71 +114,85 @@ export default function parseSlice(abqm) {
     let data = {};
     // "has" => parent/ child nodes.
     if (link.label === "has") {
-      if (!parentNodeIds.includes(link.source)) {
-        parentNodeIds.push(link.source);
-      }
-      data.parent = link.source;
-
-      if (objNodes[link.target].Class !== "NetworkService") {
-        generateDataElement(data, link.target);
-        elements.push(data)
-      }
-    }
-
-  // 'connects' => edge
-  if (link.label === "connects") {
-    // if NetworkService to CP, then add CP node inside NS's parent, don't add edge.
-    if (objNodes[link.source].Class === "NetworkService"
-      && objNodes[link.target].Class === "ConnectionPoint"
-      && objNodes[link.source].Type === "OVS") {
-      data = {
-        parent: findParentNode(link.source),
-        id: link.target,
-        label: `${link.target}.cp`,
-        type: "roundrectangle",
-        properties: { class: "Connection Point" },
-      };
-      elements.push(data);
-    } else if (objNodes[link.source].Class === "NetworkService"
-      && objNodes[link.target].Type === "ServicePort"
-      && (["L2Bridge", "L2STS"].includes(objNodes[link.source].Type))){
-        data = {
-          parent: link.source,
-          id: link.target,
-          label: `${link.target}.sp`,
-          type: "roundrectangle",
-          properties: { class: "Service Port" },
-        };
-        elements.push(data);
-      } else if (objNodes[link.target].Class === "NetworkService"
-      && objNodes[link.source].Type === "ServicePort"
-      && (["L2Bridge", "L2STS"].includes(objNodes[link.target].Type))){
-        data = {
-          parent: link.target,
-          id: link.source,
-          label: `${link.source}.sp`,
-          type: "roundrectangle",
-          properties: { class: "Service Port" },
-        };
-        elements.push(data);
-      } else if (objNodes[link.target].Class === "Link"){
-        // bypass link as node object, connect connection point directly.
-        // connect two link.source with the same link.target together.
-        !toConnectWithSameTarget[link.target] ? 
-          toConnectWithSameTarget[link.target] = [link.source] :
-          toConnectWithSameTarget[link.target].push(link.source);
-      } else if (objNodes[link.source].Class === "Link"){
-        // bypass link as node object, connect connection point directly.
-        // connect two link.source with the same link.target together.
-        !toConnectWithSameTarget[link.source] ? 
-          toConnectWithSameTarget[link.source] = [link.target] :
-          toConnectWithSameTarget[link.source].push(link.target);
+      // EXCEPTION status: OVS has SmartNIC (usually SmartNIC has OVS)
+      if (objNodes[link.target].Type === "SmartNIC" && 
+        objNodes[link.source].Type === "OVS") {
+        if (!parentNodeIds.includes(link.target)) {
+          parentNodeIds.push(link.target);
+        }
+        data.parent = link.target;
+        if (objNodes[link.target].Class !== "NetworkService") {
+          generateDataElement(data, link.target);
+          elements.push(data)
+        }
       } else {
-        data.source = link.source;
-        data.target = link.target;
-        elements.push(data);
+        if (!parentNodeIds.includes(link.source)) {
+          parentNodeIds.push(link.source);
+        }
+        data.parent = link.source;
+        if (objNodes[link.target].Class !== "NetworkService") {
+          generateDataElement(data, link.target);
+          elements.push(data)
+        }
       }
     }
+
+    // 'connects' => edge
+    if (link.label === "connects") {
+      // if NetworkService to CP, then add CP node inside NS's parent, don't add edge.
+      if (objNodes[link.source].Class === "NetworkService"
+        && objNodes[link.target].Class === "ConnectionPoint"
+        && objNodes[link.source].Type === "OVS") {
+        data = {
+          parent: findParentNode(link.source),
+          id: link.target,
+          label: `${link.target}.cp`,
+          type: "roundrectangle",
+          properties: { class: "Connection Point" },
+        };
+        console.log("----target")
+        console.log(data)
+        elements.push(data);
+      } else if (objNodes[link.source].Class === "NetworkService"
+        && objNodes[link.target].Type === "ServicePort"
+        && (["L2Bridge", "L2STS"].includes(objNodes[link.source].Type))){
+          data = {
+            parent: link.source,
+            id: link.target,
+            label: `${link.target}.sp`,
+            type: "roundrectangle",
+            properties: { class: "Service Port" },
+          };
+          elements.push(data);
+        } else if (objNodes[link.target].Class === "NetworkService"
+        && objNodes[link.source].Type === "ServicePort"
+        && (["L2Bridge", "L2STS"].includes(objNodes[link.target].Type))){
+          data = {
+            parent: link.target,
+            id: link.source,
+            label: `${link.source}.sp`,
+            type: "roundrectangle",
+            properties: { class: "Service Port" },
+          };
+          elements.push(data);
+        } else if (objNodes[link.target].Class === "Link"){
+          // bypass link as node object, connect connection point directly.
+          // connect two link.source with the same link.target together.
+          !toConnectWithSameTarget[link.target] ? 
+            toConnectWithSameTarget[link.target] = [link.source] :
+            toConnectWithSameTarget[link.target].push(link.source);
+        } else if (objNodes[link.source].Class === "Link"){
+          // bypass link as node object, connect connection point directly.
+          // connect two link.target with the same link.source together.
+          !toConnectWithSameTarget[link.source] ? 
+            toConnectWithSameTarget[link.source] = [link.target] :
+            toConnectWithSameTarget[link.source].push(link.target);
+        } else {
+          data.source = link.source;
+          data.target = link.target;
+          elements.push(data);
+        }
+      }
   })
 
   for (const linkID in toConnectWithSameTarget) {
