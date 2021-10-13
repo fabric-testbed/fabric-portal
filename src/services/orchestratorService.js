f40d14c0ed3878cc4797a10ad3f408a0c5f0275f
Yaxue Guo <yaxue.guo23@gmail.com>
2021-10-13 17:18:29 -0400

    #109: added api call to get slice by id

--- src/services/orchestratorService.js
+++ src/services/orchestratorService.js
@@ -6,5 +6,11 @@ const apiEndpoint = orchestratorApiUrl;
 export function getSlices() {
   return http.get(apiEndpoint + "?states=StableOK", {
     headers: {'Authorization': `Bearer ${localStorage.getItem("idToken")}`}
-  }).value.slices;
+  });
+}
+
+export function getSliceById(id) {
+  return http.get(apiEndpoint + "/" + id + "?graphFormat=JSON_NODELINK", {
+    headers: {'Authorization': `Bearer ${localStorage.getItem("idToken")}`}
+  });
 }
