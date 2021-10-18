17e42cf56f1b793f1312479146bfffe259d481d0
Yaxue Guo <yaxue.guo23@gmail.com>
2021-10-18 17:08:48 -0400

    #109: updated api service call

--- src/services/orchestratorService.js
+++ src/services/orchestratorService.js
@@ -4,7 +4,7 @@ import { orchestratorApiUrl } from "../config.json";
 const apiEndpoint = orchestratorApiUrl;
 
 export function getSlices() {
-  return http.get(apiEndpoint + "?states=StableOK", {
+  return http.get(apiEndpoint + "?states=['Nascent', 'Configuring', 'StableOK', 'StableError', 'Closing', 'Dead']", {
     headers: {'Authorization': `Bearer ${localStorage.getItem("idToken")}`}
   });
 }
