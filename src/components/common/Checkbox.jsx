80701942a256dbd9c2da798c4b35a74bf6b903af
Yaxue Guo <yaxue.guo23@gmail.com>
2021-10-25 17:52:08 -0400

    #109: added checkbox functionality for filtering dead slices

--- src/components/common/Checkbox.jsx
+++ src/components/common/Checkbox.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
-const Checkbox = ({ label, id, isChecked }) => {
+const Checkbox = ({ label, id, isChecked, onCheck }) => {
   return (
     <div className="form-check">
-      <input className="form-check-input" type="checkbox" value="" id={ id } checked={ isChecked }/>
+      <input
+        className="form-check-input"
+        type="checkbox"
+        value=""
+        id={ id }
+        checked={ isChecked }
+        onClick={ onCheck }
+      />
       <label className="form-check-label" for="flexCheckDefault">
         { label }
       </label>
