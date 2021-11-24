145869696497c1d9464251539f64e380d33cbbe9
Yaxue Guo <yaxue.guo23@gmail.com>
2021-11-24 12:50:08 -0500

    #99: added key card content

--- src/components/SshKey/KeyCard.jsx
+++ src/components/SshKey/KeyCard.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 
+const content = [
+  { path: "comment", label: "Name" },
+  { path: "created_on", label: "Create Date" },
+  { path: "expires_on", label: "Expiration Date" },
+  { path: "description", label: "Description" },
+  { path: "fingerprint", label: "Fingerprint" },
+  { path: "name", label: "Type" },
+  { path: "public_key", label: "Public Key"},
+]
+
 const KeyCard = ({ data, ...rest }) => {
   return (
     <div className="homepage-card card">
       <div className="card-body py-2">
-        {data.name}
+        {
+          content.map((row, index) => {
+            return (
+              <p>
+                {row.label} : {data[row.path]}
+              </p>
+            )
+          })
+        }
+        <a href="#" class="btn btn-outline-primary">Download</a>
+        <a href="#" class="btn btn-outline-primary">Delete</a>
       </div>
     </div>
   );
