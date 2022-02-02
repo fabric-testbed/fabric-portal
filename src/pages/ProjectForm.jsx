3fc9321cedb2d7d4dc740779b4a4167dfa81ec75
Yaxue Guo <yaxue.guo23@gmail.com>
2022-02-02 16:19:40 -0500

    #131: removed auto adding pm when adding po

--- src/pages/ProjectForm.jsx
+++ src/pages/ProjectForm.jsx
@@ -150,17 +150,10 @@ class projectForm extends Form {
       const originalOwners = this.state.data.project_owners;
       const owners = originalOwners;
       owners.push(user);
-      // add this user as project member in UI automatically if not member yet.
-      // (not re-render by calling api again)
-      const originalMembers = this.state.data.project_members;
-      const members = originalMembers;
-      // is not member yet, add to member from UI.
-      if (_.findIndex(members, (member) => _.isMatch(member, user)) === -1) {
-        members.push(user);
-      }
       this.setState({ data: { ...this.state.data, project_owners: owners, project_members: members } });
       try {
         await addUser("project_owner", this.state.data.uuid, user.uuid);
+        toast.success("Project owner successfully added.");
       } catch (ex) {
         console.log("failed to add project owner: " + ex.response.data);
         toast.error("Failed to add project owner.");
@@ -173,6 +166,7 @@ class projectForm extends Form {
       this.setState({ data: { ...this.state.data, project_members: members } });
       try {
         await addUser("project_member", this.state.data.uuid, user.uuid);
+        toast.success("Project member successfully added.");
       } catch (ex) {
         console.log("failed to add project member: " + ex.response.data);
         toast.error("Failed to add project member.");
