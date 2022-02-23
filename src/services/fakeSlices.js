307ee02a2b69b85c47c4ee4307002ce2d147be6c
Yaxue Guo <37635744+yaxue1123@users.noreply.github.com>
2022-02-23 13:54:10 -0500

    #167: fixed slice viewer to be compatible with updated Orchestrator API response (#147)

--- src/services/fakeSlices.js
+++ src/services/fakeSlices.js
@@ -1,23 +1,23 @@
 const slices = [
   {
-    "graph_id": "8",
-    "lease_end": "2021-09-02 14:56:57",
-    "slice_id": "8",
-    "slice_name": "slice 8",
+    "graph_id": "a8dbf297-7ba1-431e-b356-00582590c52f",
+    "lease_end": "2022-02-24 17:44:10",
+    "slice_id": "060fc906-0e7c-44a3-8a0b-48c3c83ea0f6",
+    "slice_name": "Slice-l2bridge-sriov",
     "slice_state": "StableOK"
   },
   {
-    "graph_id": "1",
-    "lease_end": "2021-11-30 19:15:53",
-    "slice_id": "1",
-    "slice_name": "Example Slice: l2ptp",
-    "slice_state": "Closing"
-  },
+    "graph_id": "f3716ea7-7ec2-4f60-982f-919555311fed",
+    "lease_end": "2022-02-24 18:10:04",
+    "slice_id": "6647deb2-fdf2-4923-b37a-7cb9c1ed13e1",
+    "slice_name": "Slice-l2bridge-mixed",
+    "slice_state": "Configuring"
+  }
 ];
 
 const sliceProperties = {
-  "8": require('./slices/slice8.json'),
   "1": require('./slices/slice1.json'),
+  "2": require('./slices/slice2.json'),
 }
 
 export function getSlices() {
