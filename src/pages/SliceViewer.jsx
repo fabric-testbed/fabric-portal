1296e5caa5651d3028221b1f620d84b3488df96e
Yaxue Guo <37635744+yaxue1123@users.noreply.github.com>
2022-03-01 16:13:42 -0500

    fixed slice viewer issue (#153)

--- src/pages/SliceViewer.jsx
+++ src/pages/SliceViewer.jsx
@@ -1,5 +1,6 @@
+
+
 import React, { Component } from 'react'
-// import SideToolbar from '../components/SliceViewer/SideToolbar';
 import Graph from '../components/SliceViewer/Graph';
 import DetailForm from '../components/SliceViewer/DetailForm';
 import _ from "lodash";
@@ -8,13 +9,19 @@ import { autoCreateTokens, autoRefreshTokens } from "../utils/manageTokens";
 import { getSliceById } from "../services/orchestratorService.js";
 import sliceParser from "../services/parser/sliceParser.js";
 import toLocaleTime from "../utils/toLocaleTime";
-
 import { toast } from "react-toastify";
 
 export default class SliceViewer extends Component { 
   state = {
     elements: [],
-    slice: {},
+    slice: {
+      "graph_id": "",
+      "lease_end": "",
+      "slice_id": "",
+      "slice_model": "",
+      "slice_name": "Slice Viewer",
+      "slice_state": "StableOK"
+    },
     selectedData: null,
     positionAddNode: { x: 100, y: 600 },
   }
@@ -48,72 +55,6 @@ export default class SliceViewer extends Component {
 
   handleNodeSelect = (selectedData) => {
     this.setState({ selectedData });
-    // ------------ TODO: ----------------
-    // console.log(selectedData)
-  }
-
-
-  handleSiteAdd = () => {
-    let eleObj = { data: { id: this.state.elements.length + 1, label: 'UKY', type: "roundrectangle", properties: {class: "Composite Node"} }, position: {x: 800, y: 250 }, classes: "graphSite"};
-    let fakeEleObj = { data: { id: this.state.elements.length + 9999, parent:  this.state.elements.length + 1, type: "roundrectangle"}, position: {x: 800, y: 350 }, classes: "fakeNode"};
-    let clonedElements = _.clone(this.state.elements);
-    clonedElements.push(eleObj);
-    clonedElements.push(fakeEleObj);
-
-    // change next node position.
-    // TODO:
-
-    this.setState({elements: clonedElements});
-  }
-
-  handleVMAdd = () => {
-    let eleObj = { data: { id: this.state.elements.length + 1, parent: 12,label: 'VM', type: "roundrectangle", properties: {class: "Composite Node"} }, position: {x: 800, y: 350 }, classes: "graphVM"};
-    let fakeEleObj = { data: { id: this.state.elements.length + 9999, parent:  this.state.elements.length + 1, type: "roundrectangle"}, position: {x: 800, y: 350 }, classes: "fakeNode"};
-    let clonedElements = _.clone(this.state.elements);
-    clonedElements.push(eleObj);
-    clonedElements.push(fakeEleObj);
-
-    // change next node position.
-    // TODO:
-
-    this.setState({elements: clonedElements});
-  }
-
-  handleNodeAdd = (type) => {
-    if (type !== "Site" && type !== "VM") {
-      let eleObj = { data: { id: this.state.elements.length + 1, type: "roundrectangle", properties: {class: "Composite Node"} }, position: this.state.positionAddNode, classes: `graph${type}` };
-      let clonedElements = _.clone(this.state.elements);
-      clonedElements.push(eleObj);
-  
-      // change next node position.
-      // TODO:
-      const x = this.state.positionAddNode.x + 110;
-      const y = this.state.positionAddNode.y;
-      this.setState({positionAddNode: { x , y }});
-  
-      this.setState({elements: clonedElements});
-    }
-  }
-
-  handleNodeDelete = (id) => {
-    const originalElements = this.state.elements;
-    const elementToDelete = originalElements.filter(e => e.data.id === id)[0];
-    let newElements = originalElements;
-    if (elementToDelete.data && elementToDelete.data.properties && elementToDelete.data.properties.interfaces) {
-      // delete all interfaces at the same time.
-      for (const interface_id of elementToDelete.data.properties.interfaces) {
-        newElements = newElements.filter(e => e.data.id !== interface_id);
-      }
-    }
-
-    // delete the node itself.
-    newElements = newElements.filter(e => e.data.id !== id);
-
-    this.setState({elements: newElements});
-  }
-
-  handleNodeUpdate = (id) => {
-    console.log(id);
   }
   
   render() {
@@ -130,7 +71,7 @@ export default class SliceViewer extends Component {
 
     return(
       <div className="mx-5 mb-4 slice-viewer-container">
-         <div className="d-flex flex-row justify-content-between align-items-center">
+         <div className="d-flex flex-row justify-content-between align-items-center mt-2">
             <h2>
               <b>{slice.slice_name}</b>
               <span className={`badge badge-${stateColors[slice.slice_state]} ml-2`}>{slice.slice_state}</span>
@@ -145,59 +86,7 @@ export default class SliceViewer extends Component {
               </button>
             </Link>
           </div>
-        {/* <div className="modal fade" id="siteModal" tabIndex="-1" role="dialog" aria-labelledby="siteModalLabel" aria-hidden="true">
-          <div className="modal-dialog" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="siteModalLabel">Add New Site</h5>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div className="modal-body">
-                <select className="custom-select" id="inputGroupSelect01">
-                  <option value="0">Choose A Site</option>
-                  <option value="1">RENC</option>
-                  <option value="2">UKY</option>
-                  <option value="3">LBNL</option>
-                </select>
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-outline-secondary" data-dismiss="modal">Close</button>
-                <button type="button" className="btn btn-outline-success" onClick={this.handleSiteAdd} data-dismiss="modal">Add</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="modal fade" id="vmModal" tabIndex="-1" role="dialog" aria-labelledby="vmModalLabel" aria-hidden="true">
-          <div className="modal-dialog" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="vmModalLabel">Add New VM</h5>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div className="modal-body">
-                <select className="custom-select" id="inputGroupSelect02">
-                  <option value="0">Add VM to Site</option>
-                  <option value="1">RENC</option>
-                  <option value="2">UKY</option>
-                  <option value="3">LBNL</option>
-                </select>
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-outline-secondary" data-dismiss="modal">Close</button>
-                <button type="button" className="btn btn-outline-success" onClick={this.handleVMAdd} data-dismiss="modal">Add</button>
-              </div>
-            </div>
-          </div>
-        </div> */}
       <div className="d-flex flex-row justify-content-center">
-        {/* <SideToolbar
-          className="align-self-start"
-          onNodeAdd={this.handleNodeAdd}
-        /> */}
         {
           elements.length > 0 &&
           <Graph
@@ -209,11 +98,7 @@ export default class SliceViewer extends Component {
         }
         {
           elements.length > 0 &&
-          <DetailForm
-            data={selectedData}
-            // onNodeDelete={this.handleNodeDelete}
-            // onNodeUpdate={this.handleNodeUpdate}
-          />
+          <DetailForm data={selectedData} />
         }
       </div>
      </div>
