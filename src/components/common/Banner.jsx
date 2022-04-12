e98f3d012a8840b9071a9a2d40ce2aba019da4b6
Yaxue Guo <37635744+yaxue1123@users.noreply.github.com>
2022-04-12 16:40:32 -0400

    Content/162 add maintenance notice (#163)
    * #162: added maintenance notice mock data
    
    * #162: pass props to notice banner
    
    * #162: added conditional rendering of notice banner across the app
    
    * #162: added new emergency notice
    
    * #162: modified style to accomodate banners on homepage
    
    * #162: modified style to accomodate banners on homepage
    
    * #162: adjusted homepage upper bg imgage height

--- src/components/common/Banner.jsx
+++ src/components/common/Banner.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Parser from 'html-react-parser';
 
-const Banner = () => {
+const Banner = (props) => {
   return (
     <div className="alert alert-warning alert-dismissible fade show" role="alert">
-      ...
+      <strong>{props.notice.title}:</strong> {Parser(props.notice.content)} &nbsp;
+      <a href={props.notice.link} target="_blank">&nbsp;&gt;&gt;&gt;More details...</a>
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
     </div>
   );
 };
