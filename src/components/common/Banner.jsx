b04ab908bc63939659399e52acd6e924365a1fc6
Yaxue Guo <yaxue.guo23@gmail.com>
2022-04-12 15:01:38 -0400

    #162: pass props to notice banner

--- src/components/common/Banner.jsx
+++ src/components/common/Banner.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Parser from 'html-react-parser';
 
-const Banner = () => {
+const Banner = (props) => {
   return (
     <div className="alert alert-warning alert-dismissible fade show" role="alert">
-      ...
+      <strong>Maintenance Notice:</strong> {Parser(props.notice.content)} &nbsp;
+      <a href={props.notice.link} target="_blank">&nbsp;&gt;&gt;&gt;More details...</a>
     </div>
   );
 };
