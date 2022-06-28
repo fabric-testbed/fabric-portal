e3c2d00993579ef8b342eec5e04a0a04f42b0a3f
Yaxue Guo <yaxue.guo23@gmail.com>
2022-06-28 15:16:56 -0400

    #193: updated /sshkeys api calls to adapt Core API

--- src/services/sshKeyService.js
+++ src/services/sshKeyService.js
@@ -1,18 +1,35 @@
 import http from './httpService';
 import { default as config } from "../config.json";
 
-const apiEndpoint = config.sshKeyApiUrl;
+const apiEndpoint = `${config.fabricCoreApiUrl}/sshkeys`;
 
 export function getActiveKeys() {
-  return http.get(`${apiEndpoint}s`);
+  const userID = localStorage.getItem("userID");
+  return http.get(`${apiEndpoint}?person_uuid=${userID}`);
 }
 
 export function uploadPublicKey(keytype, openssh, description) {
-  return http.post(`${apiEndpoint}/${keytype}?public_openssh=${encodeURIComponent(openssh)}&description=${description}`);
+  // return http.post(`${apiEndpoint}/${keytype}?public_openssh=${encodeURIComponent(openssh)}&description=${description}`);
+  return http.put({
+    url: apiEndpoint,
+    data: {
+      "description": description,
+      "keytype": keytype,
+      "public_openssh": encodeURIComponent(openssh)
+    }
+  })
 }
 
 export function generateKeyPairs(keytype, comment, description) {
-  return http.put(`${apiEndpoint}/${keytype}?comment=${comment}&description=${description}`);
+  // return http.put(`${apiEndpoint}/${keytype}?comment=${comment}&description=${description}`);
+  return http.post({
+    url: apiEndpoint,
+    data: {
+      "comment": comment,
+      "description": description,
+      "keytype": keytype
+    }
+  })
 }
 
 export function deleteKey(id) {
