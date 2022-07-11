3f7973ab30d62440231e5c1e7809660db39b5788
Yaxue Guo <yaxue.guo23@gmail.com>
2022-07-11 13:34:15 -0400

    #191: added intitial Help Page

--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -1,26 +1,51 @@
 import React from "react";
 import { default as portalData } from "../services/portalData.json";
-import { Helmet } from 'react-helmet';
 
 export default class Help extends React.Component{
-
   render() {
     return (
       <div className="container" id="contactUs">
-        <h1 className="mb-4">Getting help in FABRIC</h1>
-        {/* <Helmet>
-          <script data-jsd-embedded data-key="9779ed56-fce9-4f87-93cb-1452b94d7829" data-base-url="https://jsd-widget.atlassian.com" src="https://jsd-widget.atlassian.com/assets/embed.js"></script>
-        </Helmet> */}
-        <p>
-        For technical assistance with your experiments, please search our <a href={portalData.knowledgeBaseLink} target="_blank" rel="noopener noreferrer">Knowledge Base</a> or
-        the <a href={portalData.knowledgeBaseForumLink} target="_blank" rel="noopener noreferrer">Forums</a>. If you are unable to fix the problem on your
-        own please post a question in the appropriate section of our <a href={portalData.knowledgeBaseForumLink} target="_blank" rel="noopener noreferrer">Forums</a>.
-        </p>
-        <p>
-        If you are having problems with enrolling or logging into your FABRIC account 
-        or logging into your experiment's resources via bastion hosts, please use the form on 
-        this page or email us at <b>account-help@fabric-testbed.net</b>.
-        </p>
+        <h1 className="mb-4">Getting Help in FABRIC</h1>
+        <div className="d-flex flex-row">
+          <div className="card mr-5">
+            <div className="card-header">
+              <h3 className="mb-0"><i className="fa fa-cog"></i></h3>
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">Experiment Issues</h5>
+              <p>
+                  For technical assistance with your experiments, please search our <a href={portalData.knowledgeBaseLink} target="_blank" rel="noopener noreferrer">Knowledge Base</a> or
+                  the <a href={portalData.knowledgeBaseForumLink} target="_blank" rel="noopener noreferrer">Forums</a>. If you are unable to fix the problem on your
+                  own please post a question in the appropriate section of our <a href={portalData.knowledgeBaseForumLink} target="_blank" rel="noopener noreferrer">Forums</a>.
+                  </p>
+                  <a href={portalData.knowledgeBaseLink} target="_blank" rel="noopener noreferrer" className="btn btn-primary mr-2">
+                    <i className="fa fa-sign-in mr-2"></i>
+                    Knowledge Base
+                  </a>
+                  <a href={portalData.knowledgeBaseForumLink} target="_blank" rel="noopener noreferrer" className="btn btn-primary">
+                    <i className="fa fa-sign-in mr-2"></i>
+                    Forums
+                  </a>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-header">
+              <h3 className="mb-0"><i className="fa fa-user"></i></h3>
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">Account Issues</h5>
+              <p>
+                  If you are having problems with enrolling or logging into your FABRIC account 
+                  or logging into your experiment's resources via bastion hosts, please use the form on 
+                  this page or email us at <b>account-help@fabric-testbed.net</b>.
+                  </p>
+              <a href="https://fabric-testbed.atlassian.net/servicedesk/customer/portals" target="_blank" rel="noopener noreferrer" className="btn btn-primary">
+                <i className="fa fa-sign-in mr-2"></i>
+                FABRIC Account Help Portal
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
