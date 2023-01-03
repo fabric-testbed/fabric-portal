f20ac88a95954b926e7823da44ccd1508c2b9c47
Yaxue Guo <yaxue.guo23@gmail.com>
2023-01-03 13:00:24 -0500

    #136: updated auto-refresh token to only for project-specific tokens

--- src/utils/manageTokens.js
+++ src/utils/manageTokens.js
@@ -23,18 +23,20 @@ export const autoCreateTokens = async (projectId) => {
     localStorage.setItem("idToken", res["data"][0].id_token);
     localStorage.setItem("refreshToken", res["data"][0].refresh_token);
 
-    // autoRefreshToken after 55 minutes
-    // clear last interval if there is any
-    if(localStorage.getItem("refreshTokenIntervalId")) {
-      clearInterval(localStorage.getItem("refreshTokenIntervalId"));
+    if (projectId !== "all") {
+      // Auto refresh token every 55min
+      const refreshTokenIntervalId = setInterval(() => {
+        if(localStorage.getItem("refreshTokenIntervalId")) {
+          clearInterval(localStorage.getItem("refreshTokenIntervalId"));
+        }
+        autoRefreshTokens(projectId);
+      }
+      , portalData["autoRefreshTokenInterval"]);
+      localStorage.setItem("refreshTokenIntervalId", refreshTokenIntervalId);
+      return res["data"][0];
     }
-    // Auto refresh token every 55min
-    const refreshTokenIntervalId = setInterval(() => 
-      autoRefreshTokens(projectId)
-    , portalData["autoRefreshTokenInterval"]);
-    localStorage.setItem("refreshTokenIntervalId", refreshTokenIntervalId);
-    return res["data"][0];
-  } catch (err) {
+  }
+  catch (err) {
     toast.error("Unable to obtain authentication token, the likely reason is you are not a member of any projects.");
   }
 }
