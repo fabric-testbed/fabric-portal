f1ed0610688f5d28bac69a968a6fa9aaf26eb4e5
Yaxue Guo <yaxue.guo23@gmail.com>
2023-05-19 12:20:02 -0400

    #259: updated the wording on new sign-up page

--- src/components/Signup/DenyEnrollment.jsx
+++ src/components/Signup/DenyEnrollment.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import checkPortalType from "../../utils/checkPortalType";
 import { default as portalData } from "../../services/portalData.json";
 
 const DenyEnrollment = () => {
@@ -7,20 +6,14 @@ const DenyEnrollment = () => {
     <div>
       <div>
         <p>Dear Experimenter,</p>
-        <p>FABRIC does not support GitHub, Google, Microsoft or ORCID as an identity provider for enrollment at this time.</p>
+        <p>FABRIC does not support GitHub, Google, Microsoft or ORCID as an identity provider for enrollment without a prior agreement.</p>
         <p>
-          If your institution is not supported in the list of Identity Providers supported by CILogon you may initiate a petition to be added <a href={portalData.jiraLinks["initiateAccountPetition"]} target="_blank" rel="noreferrer">
-          using a non-institutional Identity Provider</a>.
+        If your institution is not listed among the Identity Providers supported by CILogon or you are not able to use it for some other reason, you may initiate a petition to be added via a non-institutional Identity Provider, such as GitHub, Google, Microsoft or ORCID. Please note that such petitions undergo manual review - we may request additional information prior to granting access.
         </p>
       </div>
       <div className="text-center">
       <div className="alert alert-warning" role="alert">
         <i className="fa fa-exclamation-triangle mr-2"></i> Please note that the FABRIC team works standard business hours in US Eastern timezone. We generally do not handle requests on weekends or during holidays.
-      </div>
-      <div className="alert alert-warning" role="alert">
-        <i className="fa fa-exclamation-triangle mr-2"></i>
-        Please also note that this portal is dedicated to provide solutions for a small number of well-defined problems. For all experiment-related issues, please check the available <a href={portalData.knowledgeBaseLink} target="_blank" rel="noreferrer">
-          documentation and ask questions on user forums</a> first!
       </div>
         <a href={portalData.jiraLinks["initiateAccountPetition"]}>
           <button
