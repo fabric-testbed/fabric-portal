40ebb85b9fa64a0484cea50d84beae8a2cc527fb
Yaxue Guo <yaxue.guo23@gmail.com>
2023-07-31 16:59:49 -0400

    #232: added basic slice editor UI

---
+++ src/components/SliceViewer/SliceEditorDetailForm.jsx
@@ -0,0 +1,397 @@
+import React, { Component } from 'react'
+import SingleComponent from './SingleComponent';
+import validator from  "../../utils/sliceValidator";
+
+export default class SliceEditorDetailForm extends Component {
+  state = {
+    nodeName: "",
+    isNameChanged: false,
+    core: 0,
+    isCoreChanged: false,
+    ram: 0,
+    isRamChanged: false,
+    disk: 0,
+    isDiskChanged: false,
+    BootScript: "",
+    isBootScriptChanged: false,
+    showVMComponent: false,
+    validationResult: {
+      isValid: true,
+      message: ""
+    }
+  }
+
+  // TODO: refactor code.
+  componentWillReceiveProps(props) {
+    this.setState({
+      nodeName: "",
+      isNameChanged: false,
+      core: 0,
+      isCoreChanged: false,
+      ram: 0,
+      isRamChanged: false,
+      disk: 0,
+      isDiskChanged: false,
+      showVMComponent: false,
+      validationResult: {
+        isValid: true,
+        message: ""
+      }
+    })
+  }
+
+  handleNameChange = (e) => {
+    const nodeName = e.target.value;
+    const validationResult = validator.validateDetailForm("name", nodeName, this.props.data.id, this.props.nodes);
+    this.setState({ nodeName, validationResult, isNameChanged: true });
+  }
+
+  handleCoreChange = (e) => {
+    const core = Number(e.target.value);
+    const validationResult = validator.validateDetailForm("capacity", core);
+    this.setState({ core, validationResult, isCoreChanged: true });
+  }
+
+  handleRamChange = (e) => {
+    const ram= Number(e.target.value);
+    const validationResult = validator.validateDetailForm("capacity", ram);
+    this.setState({ ram, validationResult, isRamChanged: true });
+  }
+
+  handleDiskChange = (e) => {
+    const disk = Number(e.target.value);
+    const validationResult = validator.validateDetailForm("capacity", disk);
+    this.setState({ disk, validationResult, isDiskChanged: true });
+  }
+
+  handleBootScriptChange = (e) => {
+    const BootScript = e.target.value;
+    this.setState({ BootScript, isBootScriptChanged: true });
+  }
+
+  handleShowVMComponent = () => {
+    const showOrNot = this.state.showVMComponent;
+    this.setState({ showVMComponent: !showOrNot });
+  }
+
+  handleVMUpdate = (e) => {
+    e.preventDefault();
+    const data = this.props.data;
+    const { nodeName, core, ram, disk, BootScript, isNameChanged, isCoreChanged,
+      isRamChanged, isDiskChanged, isBootScriptChanged } = this.state;
+    const newName = isNameChanged ? nodeName : data.properties.name;
+    const newCore = isCoreChanged ? core : JSON.parse(data.capacities).core;
+    const newRam = isRamChanged ? ram : JSON.parse(data.capacities).ram;
+    const newDisk = isDiskChanged ? disk : JSON.parse(data.capacities).disk;
+    const newBootScript = isBootScriptChanged ? BootScript : data.BootScript;
+    const capacities = JSON.stringify({"core": newCore, "ram": newRam, "disk": newDisk});
+    this.props.onVMUpdate({ vm_id: this.props.data.id, new_name: newName, new_capacities: capacities, new_boot_script: newBootScript });
+  }
+
+  isCPAvailable = () => {
+    const cp_id = parseInt(this.props.data.id);
+    // check if there is the CP is a 'connects' source.
+    for (const link of this.props.links) {
+      if (link.source === cp_id) {
+        return false;
+      }
+    }
+
+    // check if the CP is already selected.
+    if (this.props.selectedCPs.length === 0) {
+      return true;
+    }
+
+    for (const cp of this.props.selectedCPs) {
+      if (cp_id === parseInt(cp.id)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  handleSelect = () => {
+    this.props.onConnectionPointSelect(this.props.data);
+  }
+
+  handleNodeDelete = () => {
+    this.props.onNodeDelete(this.props.data);
+  }
+
+  handleSingleComponentAdd = (data) => {
+    this.props.onSingleComponentAdd(data);
+  }
+
+  handleFileDrop = (textStr) => {
+    this.props.onJsonUpload(textStr);
+  }
+
+  getVMComponents = () => {
+    const vm_component_ids = [];
+
+    for (const link of this.props.links) {
+      if (link.Class === "has" && link.source === parseInt(this.props.data.id)) {
+        vm_component_ids.push(link.target);
+      }
+    }
+
+    const vm_components = [];
+
+    if (vm_component_ids && vm_component_ids.length > 0) {
+      for (const compo_id of vm_component_ids) {
+        const node = this.props.nodes.filter(node => node.id === compo_id)[0];
+        const vm_component = {
+          "type": node.Type,
+          "name": node.Name,
+          "model": node.Model
+        };
+        vm_components.push(vm_component);
+      }
+    }
+
+    return vm_components;
+  }
+
+  render() {
+    const data = this.props.data;
+    let componentKey = "";
+    if (data && data.properties) {
+      componentKey = `${data.properties.id}-${data.properties.name}`
+    }
+
+    const { validationResult } = this.state;
+
+    return (
+      <div className="new-slice-detail-form" key={componentKey}>
+        <form>
+            {
+              (!data || !data.properties) && (
+                <div className="my-3">
+                    <span>
+                      <i className="fa fa-info-circle mx-2" />
+                      Click an element on the topology to make changes.
+                    </span>
+                </div>
+              )
+            }
+
+            {
+              data && data.properties && data.properties.class === "CompositeNode" &&
+              <div className="form-row px-3">
+                <div className="col mb-2">
+                  <label className="slice-builder-label">Site Name</label>
+                  <input type="text" className="form-control form-control-sm" defaultValue={data.properties.name} disabled/>
+                </div>
+              </div>
+            }
+            {
+              data && data.properties && data.properties.type === "Facility" &&
+              <div className="form-row px-3">
+                <div className="col-11 mb-2">
+                  <label className="slice-builder-label">Facility Name</label>
+                  <input type="text" className="form-control form-control-sm" defaultValue={data.properties.name} disabled/>
+                </div>
+                <div className="col-1 pt-4 pb-2 d-flex flex-row">
+                  <button
+                    className="btn btn-sm btn-danger w-100"
+                    type="button"
+                    onClick={this.handleNodeDelete}
+                  >
+                    Delete
+                  </button>
+                </div>
+                {
+                  !validationResult.isValid && validationResult.message !== "" &&
+                  <div className="mb-1 sm-alert mx-3">
+                    {validationResult.message}
+                  </div>
+                }
+              </div>
+            }
+            {
+              data && data.properties && data.properties.type === "VM" &&
+              <div>
+                <div className="form-row px-3">
+                  <div className="col-3 mb-2">
+                    <label className="slice-builder-label">VM Name</label>
+                    <input type="text" className="form-control form-control-sm" defaultValue={data.properties.name} onChange={this.handleNameChange}/>
+                  </div>
+                  <div className="col-2 mb-2">
+                    <label className="slice-builder-label">Cores</label>
+                    <input type="number" className="form-control form-control-sm" defaultValue={JSON.parse(data.capacities).core} onChange={this.handleCoreChange}/>
+                  </div>
+                  <div className="col-2 mb-2">
+                    <label className="slice-builder-label">RAM(GB)</label>
+                    <input type="number" className="form-control form-control-sm" defaultValue={JSON.parse(data.capacities).ram} onChange={this.handleRamChange}/>
+                  </div>
+                  <div className="col-2 mb-2">
+                    <label className="slice-builder-label">Disk(GB)</label>
+                    <input type="number" className="form-control form-control-sm" defaultValue={JSON.parse(data.capacities).disk} onChange={this.handleDiskChange}/>
+                  </div>
+                  <div className="col-1 pt-4 pb-2 d-flex flex-row">
+                    <button
+                      className="btn btn-sm btn-success w-100"
+                      type="button"
+                      disabled={!validationResult.isValid}
+                      onClick={this.handleVMUpdate}
+                      >
+                        Update
+                    </button>
+                  </div>
+                  <div className="col-1 pt-4 pb-2 d-flex flex-row">
+                    <button
+                      className="btn btn-sm btn-danger w-100"
+                      type="button"
+                      onClick={this.handleNodeDelete}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                  <div className="col-1 pt-4 pb-2 d-flex flex-row">
+                    <button
+                      className="btn btn-sm btn-primary w-100"
+                      type="button"
+                      onClick={this.handleShowVMComponent}
+                      >
+                        {this.state.showVMComponent ? <i className="fa fa-chevron-up"></i> : <i className="fa fa-chevron-down"></i>}
+                    </button>
+                  </div>
+                </div>
+                {!validationResult.isValid && validationResult.message !== "" &&
+                  <div className="mb-1 sm-alert mx-3">
+                    {validationResult.message}
+                  </div>
+                }
+                {
+                  this.state.showVMComponent &&
+                  <div className="form-row px-3">
+                     <div className="col-12">
+                      <label htmlFor="BootScript" className="slice-builder-label">Boot Script (optional)</label>
+                      <textarea
+                        className="form-control"
+                        id="BootScript"
+                        rows="1"
+                        defaultValue={data.BootScript}
+                        onChange={this.handleBootScriptChange}
+                      />
+                     </div>
+                     <div className="col-12">
+                      <SingleComponent
+                        addedComponents={this.getVMComponents()}
+                        onSliceComponentAdd={this.handleSingleComponentAdd}
+                      />
+                     </div>
+                  </div>
+                }
+              </div>
+            }
+
+            {
+              data && data.properties && data.properties.class === "Component" &&
+                <div className="form-row px-3">
+                  <div className="col-2 mb-2">
+                    <label className="slice-builder-label">Component Name</label>
+                    <input type="text" className="form-control form-control-sm" defaultValue={data.properties.name} disabled/>
+                  </div>
+                  <div className="col-2 mb-2">
+                    <label className="slice-builder-label">Type</label>
+                    <input type="text" className="form-control form-control-sm" defaultValue={data.properties.type} disabled/>
+                  </div>
+                  <div className="col-2 mb-2">
+                    <label className="slice-builder-label">Model</label>
+                    <input type="text" className="form-control form-control-sm" defaultValue={data.properties.model} disabled/>
+                  </div>
+                  <div className="col-5 mb-2">
+                    <label className="slice-builder-label">Detail</label>
+                    <input type="text" className="form-control form-control-sm" defaultValue={data.properties.detail} disabled/>
+                  </div>
+                  <div className="col-1 pt-4 pb-2 d-flex flex-row">
+                    <button
+                      className="btn btn-sm btn-danger ml-auto"
+                      type="button"
+                      onClick={this.handleNodeDelete}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+            }
+
+            {
+              data && data.properties && data.properties.class === "ConnectionPoint" &&
+              <div className="form-row px-3">
+                <div className="col-5 mb-2">
+                  <label className="slice-builder-label">Connection Point Name</label>
+                  <input type="text" className="form-control form-control-sm" defaultValue={ data.properties.name} disabled/>
+                </div>
+                <div className="col-5 mb-2">
+                  <label className="slice-builder-label">Type</label>
+                  <input type="text" className="form-control form-control-sm" defaultValue={data.properties.type} disabled/>
+                </div>
+                <div className="col-2 pt-4 pb-2 d-flex flex-row">
+                  {
+                    data.properties.type !== "ServicePort" && this.isCPAvailable() &&
+                    <button
+                      className="btn btn-sm btn-success ml-auto"
+                      type="button"
+                      onClick={this.handleSelect}
+                    >
+                      Select
+                    </button>
+                  }
+                   {
+                    data.properties.type === "ServicePort" && 
+                    <button
+                      className="btn btn-sm btn-danger ml-auto"
+                      type="button"
+                      onClick={this.handleNodeDelete}
+                    >
+                      Delete
+                    </button>
+                  }
+                </div>
+              </div>
+            }
+            {
+              data && data.properties && 
+              data.properties.class === "NetworkService" &&
+              data.properties.type !== "VLAN" &&
+              <div className="form-row px-3">
+                <div className="col-5 mb-2">
+                  <label className="slice-builder-label">Network Service Name</label>
+                  <input type="text" className="form-control form-control-sm" defaultValue={data.properties.name} disabled/>
+                </div>
+                <div className="col-5 mb-2">
+                  <label className="slice-builder-label">Type</label>
+                  <input type="text" className="form-control form-control-sm" defaultValue={data.properties.type} disabled/>
+                </div>
+                <div className="col-2 pt-4 pb-2 d-flex flex-row">
+                  <button
+                    className="btn btn-sm btn-danger ml-auto"
+                    type="button"
+                    onClick={this.handleNodeDelete}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            }
+            {
+              data && data.properties && data.properties.type === "VLAN" &&
+              <div className="form-row px-3">
+                <div className="col-6 mb-2">
+                  <label className="slice-builder-label">VLAN Name</label>
+                  <input type="text" className="form-control form-control-sm" defaultValue={data.properties.name} disabled/>
+                </div>
+                <div className="col-6 mb-2">
+                  <label className="slice-builder-label">Type</label>
+                  <input type="text" className="form-control form-control-sm" defaultValue={data.properties.type} disabled/>
+                </div>
+              </div>
+            }
+        </form>
+      </div>
+      );
+  };
+}
--- src/pages/SliceEditor.jsx
+++ src/pages/SliceEditor.jsx
@@ -4,32 +4,30 @@ import { v4 as uuidv4 } from 'uuid';
 import _ from "lodash";
 import { toast } from "react-toastify";
 import { saveAs } from "file-saver";
-import withRouter from "../common/withRouter.jsx";
-import SpinnerWithText from "../common/SpinnerWithText";
-import ProjectTags from "./ProjectTags";
-import SideNodes from './SideNodes';
-import SideLinks from './SideLinks';
-import Graph from './Graph';
-import SliceEditorDetailForm from './SliceEditorDetailForm';
+import withRouter from "../components/common/withRouter.jsx";
+import SpinnerWithText from "../components/common/SpinnerWithText";
+import ProjectTags from "../components/SliceViewer/ProjectTags";
+import SideNodes from '../components/SliceViewer/SideNodes';
+import SideLinks from '../components/SliceViewer/SideLinks';
+import Graph from '../components/SliceViewer/Graph';
+import SliceEditorDetailForm from '../components/SliceViewer/SliceEditorDetailForm';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
-import sliceParser from "../../services/parser/sliceParser.js";
-import builder from "../../utils/sliceBuilder.js";
-import editor from "../../utils/sliceEditor.js";
-import validator from "../../utils/sliceValidator.js";
-import { sitesNameMapping }  from "../../data/sites";
-import sitesParser from "../../services/parser/sitesParser";
-import { getResources } from "../../services/resourceService.js";
+import sliceParser from "../services/parser/sliceParser.js";
+import builder from "../utils/sliceBuilder.js";
+import editor from "../utils/sliceEditor.js";
+import validator from "../utils/sliceValidator.js";
+import { sitesNameMapping }  from "../data/sites";
+import sitesParser from "../services/parser/sitesParser";
+import { getResources } from "../services/resourceService.js";
 import { getSliceById, deleteSlice, extendSlice } from "../services/sliceService.js";
-import { autoCreateTokens } from "../../utils/manageTokens";
-import { getActiveKeys } from "../../services/sshKeyService";
-import { default as portalData } from "../../services/portalData.json";
+import { autoCreateTokens } from "../utils/manageTokens";
+import { default as portalData } from "../services/portalData.json";
 
 class SliceEditor extends React.Component {
   state = {
     sliceName: "",
     leaseEndTime: "",
     showResourceSpinner: false,
-    showKeySpinner: false,
     showSliceSpinner: false,
     sliceNodes: [],
     sliceLinks: [],
@@ -41,7 +39,10 @@ class SliceEditor extends React.Component {
   }
 
   async componentDidMount() {
-    this.setState({ showSpinner: true, spinnerText: "Loading slice..." });
+    this.setState({ 
+      showResourceSpinner: true,
+      spinnerText: "Loading resources..."
+    });
     try {
         // call credential manager to generate tokens
         autoCreateTokens(this.props.match.params.project_id).then(async () => {
@@ -60,40 +61,178 @@ class SliceEditor extends React.Component {
   }
 
   render() {
-    const { originalSlice, showSliceSpinner }
+    const { sliceName, sshKey, sliverKeys, selectedData,
+      showKeySpinner, showResourceSpinner, showSliceSpinner, parsedResources,
+      sliceNodes, sliceLinks, selectedCPs, originalSlice, elements }
     = this.state;
 
+    const validationResult = validator.validateSlice(sliceName, sshKey, sliceNodes);
+
+    const renderTooltip = (id, content) => (
+      <Tooltip id={id}>
+        {content}
+      </Tooltip>
+    );
+    
     return (
       <div>
-      {
-        !showSliceSpinner && 
-        <div>
-          <div className="d-flex flex-row justify-content-between mt-2">
-            <div className="align-self-start d-flex flex-row">
-              <h2 className="ml-5 my-2">
-                { originalSlice.name }
-              </h2>
-              <a
-                href={portalData.learnArticles.guideToSliceBuilder}
-                target="_blank"
-                rel="noreferrer"
-                className="mt-3"
-              >
-                <i className="fa fa-question-circle mx-2"></i>
-                User Guide
-              </a>
+        {
+          !showResourceSpinner&& 
+          <div>
+            <div className="d-flex flex-row justify-content-between mt-2">
+              <div className="align-self-start d-flex flex-row">
+                <h2 className="ml-5 my-2">
+                  { originalSlice.name }
+                </h2>
+                <a
+                  href={portalData.learnArticles.guideToSliceBuilder}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="mt-3"
+                >
+                  <i className="fa fa-question-circle mx-2"></i>
+                  User Guide
+                </a>
+              </div>
+              <Link to={`/projects/${this.props.match.params.project_id}`} className="align-self-end mr-5">
+                <button
+                  className="btn btn-sm btn-outline-primary my-3"
+                >
+                  <i className="fa fa-exchange mr-2"></i>
+                  View Mode
+                </button>
+              </Link>
             </div>
-            <Link to={`/projects/${this.props.match.params.project_id}`} className="align-self-end mr-5">
-              <button
-                className="btn btn-sm btn-outline-primary my-3"
-              >
-                <i className="fa fa-exchange mr-2"></i>
-                View Mode
-              </button>
-            </Link>
           </div>
+        }
+        <div className="d-flex flex-column align-items-center w-100">
+          <div className="d-flex flex-row justify-content-center mb-4 w-100 mx-5">
+            <div className="slice-builder-left">
+            <div className="card">
+                <div>
+                  <div className="card-body slice-builder-card-body">
+                    <ProjectTags projectId={this.props.match.params.project_id} />
+                  </div>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-header slice-builder-card-header py-1">
+                  <a
+                    href={portalData.learnArticles.guideToSliceBuilderSections["step2"]}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <button className="btn btn-link">
+                      Add Topology Elements
+                    </button>
+                  </a>
+                </div>
+                <div>
+                  <div className="card-body slice-builder-card-body">
+                    {
+                      showResourceSpinner && <SpinnerWithText text={"Loading site resources..."}/>
+                    }
+                    {
+                      !showResourceSpinner && 
+                      <SideNodes
+                        resources={parsedResources}
+                        nodes={sliceNodes}
+                        onVMAdd={this.handleVMAdd}
+                        onFacilityAdd={this.handleFacilityAdd}
+                      />
+                    }
+                  </div>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-header slice-builder-card-header py-1">
+                  <a
+                    href={portalData.learnArticles.guideToSliceBuilderSections["step3"]}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <button className="btn btn-link">
+                      Add Network Services
+                    </button>
+                  </a>
+                </div>
+                <div>
+                  <div className="card-body slice-builder-card-body">
+                  <SideLinks
+                    nodes={sliceNodes}
+                    selectedCPs={selectedCPs}
+                    onLinkAdd={this.handleLinkAdd}
+                    onCPRemove={this.handleCPRemove}
+                  />
+                  </div>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-header slice-builder-card-header py-1">
+                  <a
+                    href={portalData.learnArticles.guideToSliceBuilderSections["step4"]}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <button className="btn btn-link">
+                      Submit Request
+                    </button>
+                  </a>
+                </div>
+                <div>
+                  <div className="card-body slice-builder-card-body">
+                    <div className="d-flex flex-column">
+                      <form>
+                        <div className="form-group col-md-12 d-flex flex-row">
+                          <OverlayTrigger
+                            placement="top"
+                            delay={{ show: 100, hide: 300 }}
+                            overlay={renderTooltip("slice-create-tooltip",
+                              "The slice graph will be automatically saved to Draft when creating slice.")}
+                          >
+                            <button
+                              className="btn btn-sm btn-success mt-2"
+                              type="button"
+                              disabled={!validationResult.isValid}
+                              onClick={() => this.handleCreateSlice()}
+                            >
+                              Modify Slice
+                            </button>
+                        </OverlayTrigger>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              </div>
+              <div className="slice-builder-right">
+              <SliceEditorDetailForm
+                data={selectedData}
+                selectedCPs={selectedCPs}
+                nodes={sliceNodes}
+                links={sliceLinks}
+                onConnectionPointSelect={this.handleCPAdd}
+                onNodeDelete={this.handleNodeDelete}
+                onVMUpdate={this.handleVMUpdate}
+                onSingleComponentAdd={this.handleSingleComponentAdd}
+                onJsonUpload={this.handleJsonUpload}
+              />
+              <Graph
+                className="align-self-end"
+                isNewSlice={true}
+                elements={this.generateGraphElements()}
+                sliceName={sliceName === "" ? "new-slice" : sliceName}
+                defaultSize={{"width": 0.6, "height": 0.9, "zoom": 1}}
+                onNodeSelect={this.handleNodeSelect}
+                onSaveDraft={() => this.handleSaveDraft("withMessage")}
+                onUseDraft={this.handleUseDraft}
+                onClearGraph={this.handleClearGraph}
+                onSaveJSON={this.handleSaveJSON}
+              />
+            </div>
+            </div>
         </div>
-      }
       </div>
     )
   }
