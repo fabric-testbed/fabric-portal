3a88bd0607fff4ea4fd8ee10b79cef89683e3721
Yaxue Guo <yaxue.guo23@gmail.com>
2023-10-30 12:28:09 -0400

    #296: replaced mock data with API call for carousel items

--- src/components/Home/Carousel.jsx
+++ src/components/Home/Carousel.jsx
@@ -1,32 +1,25 @@
 import React from "react";
 import { getActiveCarouselItems } from "../../services/announcementService";
 import { toast } from "react-toastify";
-import { NavLink } from "react-router-dom";
-import bg1 from "../../imgs/homepage/bg1.jpeg";
-import bg2 from "../../imgs/homepage/bg2.jpeg";
-import bg3 from "../../imgs/homepage/bg3.jpeg";
+import Parser from 'html-react-parser';
 
 class Carousel extends React.Component {
   state = {
-    showSpinner: false,
     items: [],
   };
 
   async componentDidMount() {
-    this.setState({ showSpinner: true });
-
     try {
       const { data: res } = await getActiveCarouselItems();
       let items = res.results;
-      this.setState({ items, showSpinner: false });
+      this.setState({ items });
     } catch (err) {
-      this.setState({ showSpinner: false });
       toast.error("Failed to load carousel items. Please reload this page.");
     }
   }
 
   render() {
-    const { items, showSpinner } = this.state;
+    const { items } = this.state;
     return (
       <div
       id="homepageCarouselIndicators"
@@ -39,62 +32,30 @@ class Carousel extends React.Component {
         <li data-target="#homepageCarouselIndicators" data-slide-to="2"></li>
       </ol>
       <div className="carousel-inner">
-        <div className="carousel-item active">
-          <img src={bg1} alt={`FABRIC Portal Homepage Slide 1`} className="d-block w-100"/>
-          <div className="carousel-caption d-none d-md-block">
-            <div className="carousel-caption-content">
-              <h3>FABRIC Portal is your guide, helping make your experiment a success.</h3>
-              <ul>
-                <li>Build Community: Inspire others with your research, discover collaborators, and find opportunities to showcase your project.</li>
-                <li>Conduct Experiments: Take advantage of FABRIC resources to design, deploy, execute, and monitor your experiments.</li>
-                <li>Browse the Library: Learn more about FABRIC through publications and user documentation. Discover additional complimentary facilities and testbeds to expand your research.</li>
-              </ul>
-              <NavLink to="/about/about-fabric">
-                <button className="btn btn-sm btn-warning">Learn More</button>
-              </NavLink>
+        {
+          items && items.length > 0 && items.map((item, index) =>
+          <div className={`carousel-item ${item.sequence === 1? "active" : ""}`}>
+            <img src={item.background_image_url} alt={`FABRIC Portal Homepage Slide ${index}`} className="d-block w-100"/>
+            <div className="carousel-caption d-none d-md-block">
+              <div className="carousel-caption-content">
+                <h3>{item.title}</h3>
+                <div className="homepage-carousel-content">
+                  {Parser(item.content)}
+                </div>
+                <a
+                  href={item.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="btn btn-sm btn-warning">
+                    {item.button}
+                  </button>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="carousel-item">
-          <img src={bg2} alt={`FABRIC Portal Homepage Slide 2`} className="d-block w-100"/>
-          <div className="carousel-caption d-none d-md-block">
-            <div className="carousel-caption-content">
-              <h3>
-                NGI Enrichers program seeks host organizations for funded researchers
-              </h3>
-              <p>
-                Next Generation Internet (NGI) Enrichers is an initiative that supports transatlantic research cooperation in areas related to the next generation of the internet, such as networking, cybersecurity, virtual reality, 5G, machine learning, and several others.
-              </p>
-              <a
-                href="https://fabric-testbed.net/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <button className="btn btn-sm btn-warning">Learn More</button>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="carousel-item">
-          <img src={bg3} alt={`FABRIC Portal Homepage Slide 3`} className="d-block w-100"/>
-          <div className="carousel-caption d-none d-md-block">
-            <div className="carousel-caption-content">
-              <h3>
-                NSF FABRIC Project Completes Phase 1, Enabling Early Testing of Unprecedented Large-scale Network Experiments
-              </h3>
-              <p>
-                The NSF-funded FABRIC project has made steady progress establishing the groundbreaking network testbed infrastructure to reimagine the way large amounts of data are generated, stored, analyzed, and transmitted across the world.
-              </p>
-              <a
-                href="https://fabric-testbed.net/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <button className="btn btn-sm btn-warning">Learn More</button>
-              </a>
-            </div>
-          </div>
-        </div>
+        )
+        }
       </div>
       <a className="carousel-control-prev" href="#homepageCarouselIndicators" role="button" data-slide="prev">
         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
