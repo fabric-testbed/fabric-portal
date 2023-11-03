790434c8c9df8be3d5ece40d9c7afed69cf19202
Yaxue Guo <yaxue.guo23@gmail.com>
2023-11-03 15:27:31 -0400

    #306: updated slice page conditional rendering

--- src/components/Experiment/Slices.jsx
+++ src/components/Experiment/Slices.jsx
@@ -207,72 +207,66 @@ class Slices extends React.Component {
           </div>
         }
         {
-          (!showSpinner && hasProject && slices.length > 0) ?
+          !showSpinner && hasProject && slices.length === 0 &&
+          this.props.parent === "Projects" && 
+          !this.props.isProjectExpired &&
             <div>
-              {
-              this.props.parent === "Projects" && 
-              !this.props.isProjectExpired &&
-              <div>
-                <div className="d-flex flex-row">
-                  <Link to={`/new-slice/${this.props.projectId}`} className="btn btn-primary mr-4">
-                    Create Slice in Portal
-                  </Link>
-                  <a
-                    href={this.jupyterLinkMap[checkPortalType(window.location.href)]}
-                    className="btn btn-primary"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Create Slice in JupyterHub
-                  </a>
-                </div>
-                <div className="alert alert-warning mt-3" role="alert">
-                  <p className="mt-2">
-                    You have no slices in this project. Please create slices in Portal or&nbsp;
-                    <a
-                    href={this.jupyterLinkMap[checkPortalType(window.location.href)]}
-                    target="_blank"
-                    rel="noreferrer"
-                    >JupyterHub</a> first. Here are some guide articles you may find helpful:
-                  </p>
-                  <p>
-                    <ul>
-                      <li><a href={portalData.learnArticles.guideToSliceBuilder} target="_blank" rel="noreferrer">Portal Slice Builder User Guide</a></li>
-                      <li><a href={portalData.learnArticles.guideToStartExperiment} target="_blank" rel="noreferrer">Start Your First Experiment</a></li>
-                      <li><a href={portalData.learnArticles.guideToInstallPythonAPI} target="_blank" rel="noreferrer">Install the FABRIC Python API</a></li>
-                      <li><a href={portalData.learnArticles.guideToSliceManager} target="_blank" rel="noreferrer">Slice Manager</a></li>
-                      <li><a href={portalData.learnArticles.guideToSliceEditor} target="_blank" rel="noreferrer">Slice Editor</a></li>
-                    </ul>
-                  </p>
-                </div>
+              <div className="d-flex flex-row">
+                <Link to={`/new-slice/${this.props.projectId}`} className="btn btn-primary mr-4">
+                  Create Slice in Portal
+                </Link>
+                <a
+                  href={this.jupyterLinkMap[checkPortalType(window.location.href)]}
+                  className="btn btn-primary"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Create Slice in JupyterHub
+                </a>
               </div>
-              }
+              <div className="alert alert-warning mt-3" role="alert">
+                <p className="mt-2">
+                  You have no slices in this project. Please create slices in Portal or&nbsp;
+                  <a
+                  href={this.jupyterLinkMap[checkPortalType(window.location.href)]}
+                  target="_blank"
+                  rel="noreferrer"
+                  >JupyterHub</a> first. Here are some guide articles you may find helpful:
+                </p>
+                <p>
+                  <ul>
+                    <li><a href={portalData.learnArticles.guideToSliceBuilder} target="_blank" rel="noreferrer">Portal Slice Builder User Guide</a></li>
+                    <li><a href={portalData.learnArticles.guideToStartExperiment} target="_blank" rel="noreferrer">Start Your First Experiment</a></li>
+                    <li><a href={portalData.learnArticles.guideToInstallPythonAPI} target="_blank" rel="noreferrer">Install the FABRIC Python API</a></li>
+                    <li><a href={portalData.learnArticles.guideToSliceManager} target="_blank" rel="noreferrer">Slice Manager</a></li>
+                    <li><a href={portalData.learnArticles.guideToSliceEditor} target="_blank" rel="noreferrer">Slice Editor</a></li>
+                  </ul>
+                </p>
+              </div>
             </div>
-            :
-            <div>
-              {
-                !showSpinner && 
-                <div className="alert alert-warning mt-3" role="alert">
-                  <p className="mt-2">
-                    We couldn't find any slices belonging to you. Please create slices in Portal or &nbsp;
-                    <a
-                    href={this.jupyterLinkMap[checkPortalType(window.location.href)]}
-                    target="_blank"
-                    rel="noreferrer"
-                    >JupyterHub</a> first. Here are some guide articles you may find helpful:
-                  </p>
-                  <p>
-                    <ul>
-                      <li><a href={portalData.learnArticles.guideToSliceBuilder} target="_blank" rel="noreferrer">Portal Slice Builder User Guide</a></li>
-                      <li><a href={portalData.learnArticles.guideToStartExperiment} target="_blank" rel="noreferrer">Start Your First Experiment</a></li>
-                      <li><a href={portalData.learnArticles.guideToInstallPythonAPI} target="_blank" rel="noreferrer">Install the FABRIC Python API</a></li>
-                      <li><a href={portalData.learnArticles.guideToSliceManager} target="_blank" rel="noreferrer">Slice Manager</a></li>
-                      <li><a href={portalData.learnArticles.guideToSliceEditor} target="_blank" rel="noreferrer">Slice Editor</a></li>
-                    </ul>
-                  </p>
-                </div>
-              }
-            </div>
+        }
+        {
+          !showSpinner && hasProject && slices.length === 0 &&
+          this.props.parent !== "Projects" && 
+          <div className="alert alert-warning mt-3" role="alert">
+            <p className="mt-2">
+              We couldn't find any slices belonging to you. Please create slices in Portal or &nbsp;
+              <a
+              href={this.jupyterLinkMap[checkPortalType(window.location.href)]}
+              target="_blank"
+              rel="noreferrer"
+              >JupyterHub</a> first. Here are some guide articles you may find helpful:
+            </p>
+            <p>
+              <ul>
+                <li><a href={portalData.learnArticles.guideToSliceBuilder} target="_blank" rel="noreferrer">Portal Slice Builder User Guide</a></li>
+                <li><a href={portalData.learnArticles.guideToStartExperiment} target="_blank" rel="noreferrer">Start Your First Experiment</a></li>
+                <li><a href={portalData.learnArticles.guideToInstallPythonAPI} target="_blank" rel="noreferrer">Install the FABRIC Python API</a></li>
+                <li><a href={portalData.learnArticles.guideToSliceManager} target="_blank" rel="noreferrer">Slice Manager</a></li>
+                <li><a href={portalData.learnArticles.guideToSliceEditor} target="_blank" rel="noreferrer">Slice Editor</a></li>
+              </ul>
+            </p>
+          </div>
         }
         {
           !showSpinner && hasProject && slices.length > 0 && <div>
