e936aa9cc42b827536dce8513c00cd9a7e7d0a35
Yaxue Guo <yaxue.guo23@gmail.com>
2024-01-16 12:30:51 -0500

    #313: added spinner for deleting slice

--- src/pages/SliceViewer.jsx
+++ src/pages/SliceViewer.jsx
@@ -79,17 +79,13 @@ class SliceViewer extends Component {
   }
 
   handleDeleteSlice = async (id) => {
+    this.setState({ showSpinner: true, spinnerText: "Deleting Slice" });
     try {
       await deleteSlice(id);
       // toast message to users when the api call is successfully done.
       toast.success("Slice deleted successfully.");
-      // change slice state to Dead.
-      this.setState(prevState => ({ 
-        slice: {
-          ...prevState.slice,
-          "state": "Dead"
-        }
-      }))
+      await sleep(1000);
+      window.location.reload();
     } catch (err) {
       toast.error("Failed to delete the slice.");
     }
