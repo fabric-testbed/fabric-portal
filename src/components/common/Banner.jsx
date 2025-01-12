e1baa22a4959a568bc5c3e5c4b852a88b0d74c46
Yaxue Guo <yaxue.guo23@gmail.com>
2025-01-12 01:14:42 -0500

    #365: updated style for maintenance banner close button

--- src/components/common/Banner.jsx
+++ src/components/common/Banner.jsx
@@ -6,8 +6,7 @@ const Banner = (props) => {
     <div className="alert alert-warning alert-dismissible fade show notice-banner" role="alert">
       <strong>{props.notice.title}:</strong> {Parser(props.notice.content)} &nbsp;
       { props.notice.link && <a href={props.notice.link} target="_blank" rel="noreferrer">&nbsp;&gt;&gt;&gt;More details...</a> }
-      <button type="button" className="close" data-bs-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
+      <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close">
       </button>
     </div>
   );
